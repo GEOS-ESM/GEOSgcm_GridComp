fb695bdf1caf883855aed37be85abf1501b376ee
Yury Vikhliaev <yury.v.vikhliaev@nasa.gov>
2020-02-28 15:48:00 -0500

    Trying to run MOM6. WIP

--- GEOSogcm_GridComp/GEOSocean_GridComp/GuestOcean_GridComp/GuestOcean_GridComp.F90
+++ GEOSogcm_GridComp/GEOSocean_GridComp/GuestOcean_GridComp/GuestOcean_GridComp.F90
@@ -544,11 +544,11 @@ contains
             CHILD_ID   = OCN,                                         &
             RC=STATUS  )
        VERIFY_(STATUS)
-       call MAPL_AddExportSpec ( GC   ,                          &
-            SHORT_NAME = 'SSH',                                       &
-            CHILD_ID   = OCN,                                         &
-            RC=STATUS  )
-       VERIFY_(STATUS)
+!!$       call MAPL_AddExportSpec ( GC   ,                          &
+!!$            SHORT_NAME = 'SSH',                                       &
+!!$            CHILD_ID   = OCN,                                         &
+!!$            RC=STATUS  )
+!!$       VERIFY_(STATUS)
        call MAPL_AddExportSpec ( GC   ,                          &
             SHORT_NAME = 'SLV',                                       &
             CHILD_ID   = OCN,                                         &
@@ -559,11 +559,11 @@ contains
             CHILD_ID   = OCN,                                         &
             RC=STATUS  )
        VERIFY_(STATUS)
-       call MAPL_AddExportSpec ( GC   ,                               &
-            SHORT_NAME = 'PBO',                                       &
-            CHILD_ID   = OCN,                                         &
-            RC=STATUS  )
-       VERIFY_(STATUS)
+!!$       call MAPL_AddExportSpec ( GC   ,                               &
+!!$            SHORT_NAME = 'PBO',                                       &
+!!$            CHILD_ID   = OCN,                                         &
+!!$            RC=STATUS  )
+!!$       VERIFY_(STATUS)
 
        call MAPL_AddExportSpec ( GC   ,                          &
             SHORT_NAME = 'T',                                         &
