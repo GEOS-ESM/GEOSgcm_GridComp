a46c0aecdac72b8a4bde6b4c7eb164678b8cdb5e
Sarith Mahanama <sarith.p.mahanama@nasa.gov>
2020-09-28 11:41:57 -0400

    updated for LAND_FIX

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Shared/SurfParams.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Shared/SurfParams.F90
@@ -51,118 +51,107 @@ module SurfParams
       
     if (LSM_CHOICE==1) then   
     
-    select case (LAND_PARAMS)
-      case ("Icarus")  ! "Old" LDASsa physics, current default for Icarus GCM
-         LAND_FIX = .FALSE. 
-         CSOIL_2  = 200.  
-         WEMIN    = 26.   
-         AICEV    = 0.149 
-         AICEN    = 19.851
-         FLWALPHA = 1.      ! i.e., FLWALPHA unchanged
-         ASTRFR   = 0.333
-         STEXP    = 1.
-         RSWILT   = 500.
+	select case (LAND_PARAMS)
+	case ("Icarus")  ! "Old" LDASsa physics, current default for Icarus GCM
+		LAND_FIX = .FALSE. 
+		CSOIL_2  = 200.  
+		WEMIN    = 26.   
+		AICEV    = 0.149 
+		AICEN    = 19.851
+		FLWALPHA = 1.      ! i.e., FLWALPHA unchanged
+		ASTRFR   = 0.333
+             	STEXP    = 1.
+             	RSWILT   = 500.
 
-      case ("V24_C05")  ! V24_C05 changes, default for LDAS m4-17-0
-         LAND_FIX = .TRUE.
-         CSOIL_2  = 70000.  ! Post H5_0 
-         WEMIN    = 13.   
-         AICEV    = 0.107 
-         AICEN    = 19.893
-         FLWALPHA = 0.01
-         ASTRFR   = 1.
-         STEXP    = 2.
-         RSWILT   = 2000.
+	case ("V24_C05")  ! V24_C05 changes, default for LDAS m4-17-0
+         	LAND_FIX = .TRUE.
+         	CSOIL_2  = 70000.  ! Post H5_0 
+         	WEMIN    = 13.   
+         	AICEV    = 0.107 
+         	AICEN    = 19.893
+         	FLWALPHA = 0.01
+         	ASTRFR   = 1.
+         	STEXP    = 2.
+         	RSWILT   = 2000.
 
-      case ("NRv7.2")  ! SMAP NRv7.2 changes, default for after LDAS m4-17-6
-         LAND_FIX = .TRUE. 
-         CSOIL_2  = 70000. ! Post H5_0 
-         WEMIN    = 13.     
-         AICEV    = 0.149 
-         AICEN    = 19.851
-         FLWALPHA = 0.04
-         ASTRFR   = 0.333  ! reverted 
-         STEXP    = 1.     ! reverted
-         RSWILT   = 500.   ! reverted
+    	case ("NRv7.2")  ! SMAP NRv7.2 changes, default for after LDAS m4-17-6
+         	LAND_FIX = .TRUE. 
+         	CSOIL_2  = 70000. ! Post H5_0 
+         	WEMIN    = 13.     
+         	AICEV    = 0.149 
+         	AICEN    = 19.851
+         	FLWALPHA = 0.04
+         	ASTRFR   = 0.333  ! reverted 
+        	STEXP    = 1.     ! reverted
+         	RSWILT   = 500.   ! reverted
 
-      case DEFAULT                                                                            
-         write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
-         return                                                                               
-      end select                                                                                
+      	case DEFAULT                                                                            
+         	write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
+         	return                                                                               
+      	end select                                                                                
                                                                                               
     else if (LSM_CHOICE==2) then       
 
-       select case (LAND_PARAMS)                                                                
-       case ("e0004s_transientCO2_05")  ! parameters to reproduce Fanwei Zeng's Catchment-CN.4.0 runs (e0004s_transientCO2_05) done with build /gpfsm/dnb31/fzeng/LDASsa_m3-16_0_p2_CatchCatchCN_for_MERRA3        
-         LAND_FIX = .TRUE.                                                                    
-         LAND_FIX_CN = .FALSE.                                                                
-         CSOIL_2  = 70000. ! Post H5_0  
-         WEMIN    = 13.                                                                       
-         AICEV    = 0.149                                                                     
-         AICEN    = 19.851                                                                    
-         FLWALPHA = 1.                                                                        
-         ASTRFR   = 0.333  ! reverted 
-         STEXP    = 1.     ! reverted                                  
-         RSWILT   = 1500.   
+	select case (LAND_PARAMS)                                                                
+       	case ("CN_CLM4")  ! parameters to reproduce Fanwei Zeng's Catchment-CN.4.0 runs (e0004s_transientCO2_05) done with build /gpfsm/dnb31/fzeng/LDASsa_m3-16_0_p2_CatchCatchCN_for_MERRA3        
+         	LAND_FIX = .TRUE.                                                                    
+         	LAND_FIX_CN = .FALSE.                                                                
+         	CSOIL_2  = 70000. ! Post H5_0  
+         	WEMIN    = 13.                                                                       
+         	AICEV    = 0.149                                                                     
+         	AICEN    = 19.851                                                                    
+         	FLWALPHA = 1.                                                                        
+         	ASTRFR   = 0.333  ! reverted 
+         	STEXP    = 1.     ! reverted                                  
+         	RSWILT   = 1500.   
 
-       case ("Icarus")  ! "Old" LDASsa physics, current default for Icarus GCM                
-         LAND_FIX = .FALSE.                                                                   
-         LAND_FIX_CN = .TRUE.                                                                
-         CSOIL_2  = 200.                                                                      
-         WEMIN    = 26.                                                                       
-         AICEV    = 0.149                                                                     
-         AICEN    = 19.851                                                                    
-         FLWALPHA = 0.005      ! set to previously hardcoded CatchCN value                    
-         ASTRFR   = 0.333                                                                     
-         STEXP    = 1.                                                                        
-         RSWILT   = 2000.      ! set to previously hardcoded CatchCN value                    
+       	case ("Icarus")  ! "Old" LDASsa physics, current default for Icarus GCM                
+         	LAND_FIX = .FALSE.                                                                   
+         	LAND_FIX_CN = .TRUE.                                                                
+         	CSOIL_2  = 200.                                                                      
+         	WEMIN    = 26.                                                                       
+         	AICEV    = 0.149                                                                     
+         	AICEN    = 19.851                                                                    
+         	FLWALPHA = 0.005      ! set to previously hardcoded CatchCN value                    
+         	ASTRFR   = 0.333                                                                     
+         	STEXP    = 1.                                                                        
+         	RSWILT   = 2000.      ! set to previously hardcoded CatchCN value                    
                                                                                               
-      case ("V24_C05")  ! V24_C05 changes, default for LDAS m4-17-0                           
-         LAND_FIX = .TRUE.                                                                    
-         LAND_FIX_CN = .TRUE.                                                                 
-         CSOIL_2  = 70000.  ! Post H5_0                                                       
-         WEMIN    = 13.                                                                       
-         AICEV    = 0.107                                                                     
-         AICEN    = 19.893
-         FLWALPHA = 0.005   ! set to previously hardcoded CatchCN value
-         ASTRFR   = 1.                                                                        
-         STEXP    = 2.                                                                        
-         RSWILT   = 2000.   ! set to previously hardcoded CatchCN value   
+      	case ("V24_C05")  ! V24_C05 changes, default for LDAS m4-17-0                           
+         	LAND_FIX = .TRUE.                                                                    
+         	LAND_FIX_CN = .TRUE.                                                                 
+         	CSOIL_2  = 70000.  ! Post H5_0                                                       
+         	WEMIN    = 13.                                                                       
+         	AICEV    = 0.107                                                                     
+         	AICEN    = 19.893
+         	FLWALPHA = 0.005   ! set to previously hardcoded CatchCN value
+         	ASTRFR   = 1.                                                                        
+         	STEXP    = 2.                                                                        
+         	RSWILT   = 2000.   ! set to previously hardcoded CatchCN value   
 
-      case ("NRv7.2")  ! SMAP NRv7.2 changes, default for after LDAS m4-17-6                  
-         LAND_FIX = .TRUE.                                                                    
-         LAND_FIX_CN = .TRUE.                                                                 
-         CSOIL_2  = 70000. ! Post H5_0                                                        
-         WEMIN    = 13.                                                                       
-         AICEV    = 0.149
-         AICEN    = 19.851                                                                    
-         FLWALPHA = 0.005  ! set to previously hardcoded CatchCN value                           
-         ASTRFR   = 0.333  ! reverted  
-         STEXP    = 1.     ! reverted     
-         RSWILT   = 2000.  ! set to previously hardcoded CatchCN value                        
+      	case ("NRv7.2")  ! SMAP NRv7.2 changes, default for after LDAS m4-17-6                  
+         	LAND_FIX = .TRUE.                                                                    
+         	LAND_FIX_CN = .TRUE.                                                                 
+         	CSOIL_2  = 70000. ! Post H5_0                                                        
+         	WEMIN    = 13.                                                                       
+         	AICEV    = 0.149
+         	AICEN    = 19.851                                                                    
+         	FLWALPHA = 0.005  ! set to previously hardcoded CatchCN value                           
+         	ASTRFR   = 0.333  ! reverted  
+         	STEXP    = 1.     ! reverted     
+         	RSWILT   = 2000.  ! set to previously hardcoded CatchCN value                        
                                                                                               
-      case ("EL_CLM45_Fire")  ! parameters to reproduce Eunjee Lee's CatchCN_CLM4.5 simulations of GPP and fire carbon emissions        
-         LAND_FIX = .TRUE.
-         LAND_FIX_CN = .FALSE.
-         CSOIL_2  = 70000. ! Post H5_0  
-         WEMIN    = 13.
-         AICEV    = 0.107
-         AICEN    = 19.893
-         FLWALPHA = 0.005
-         ASTRFR   = 0.333  ! reverted 
-         STEXP    = 1.     ! reverted                                  
-         RSWILT   = 2000.
-      case DEFAULT                                                                            
-         write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
-         return                                                                               
-      end select     
+      	case DEFAULT                                                                            
+         	write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
+         	return                                                                               
+      	end select     
     else                                                                                      
-        write (*,*) "invalid land model choice ",LSM_CHOICE                                                   
-         return                                                                               
+      	write (*,*) "invalid land model choice ",LSM_CHOICE                                                   
+     	 return                                                                               
     end if ! LSM_CHOICE  
 
     init_called = .TRUE.
 
   end subroutine SurfParams_init
 
-end module SurfParams
+endmodule SurfParams
