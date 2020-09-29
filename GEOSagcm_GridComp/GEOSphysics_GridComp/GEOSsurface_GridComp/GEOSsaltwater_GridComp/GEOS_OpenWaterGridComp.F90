1e64011b17da73ac8f7768f952aa526e311552ba
sanAkel <santha.akella@nasa.gov>
2020-09-29 19:46:14 -0400

    send TS as rank-1 to SKIN_SST

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSsaltwater_GridComp/GEOS_OpenWaterGridComp.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSsaltwater_GridComp/GEOS_OpenWaterGridComp.F90
@@ -2757,7 +2757,7 @@ contains
       call SKIN_SST (DO_SKIN_LAYER, DO_DATASEA, NT,CM,UUA,VVA,UW,VW,HW,SWN,LHF,SHF,LWDNSRF,                   &
                      ALW,BLW,PEN, PEN_OCEAN, STOKES_SPEED,DT,MUSKIN,TS_FOUNDi,DWARM_,TBAR_,TXW,TYW,USTARW_,   &
                      DCOOL_,TDROP_,SWCOOL_,QCOOL_,BCOOL_,LCOOL_,TDEL_,SWWARM_,QWARM_,ZETA_W_,                 &
-                     PHIW_,LANGM_,TAUTW_,uStokes_,TS,TWMTS,TW,WATER,FRWATER,n_iter_cool,                         &
+                     PHIW_,LANGM_,TAUTW_,uStokes_,TS(:,WATER),TWMTS,TW,WATER,FRWATER,n_iter_cool,                         &
                      fr_ice_thresh, epsilon_d, trim(DO_GRAD_DECAY_warmLayer))
 
 ! Copies for export
