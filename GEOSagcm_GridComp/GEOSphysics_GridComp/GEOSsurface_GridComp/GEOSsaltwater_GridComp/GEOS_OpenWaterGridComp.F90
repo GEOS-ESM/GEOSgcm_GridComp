e61a41fe0e630c1cb81cc4f75ef9cde79db8837e
sanAkel <santha.akella@nasa.gov>
2020-10-01 11:43:19 -0400

    Update internal variables: TWMTF, DELTC following update of TW and TDROP respectively. Commented only for sake of zero-diff.

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSsaltwater_GridComp/GEOS_OpenWaterGridComp.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSsaltwater_GridComp/GEOS_OpenWaterGridComp.F90
@@ -2756,7 +2756,7 @@ contains
       call SKIN_SST (DO_SKIN_LAYER, DO_DATASEA, NT,CM(:,WATER),UUA,VVA,UW,VW,HW,SWN_surf,LHF,SHF,LWDNSRF,     &
                      ALW,BLW,PEN, PEN_OCEAN, STOKES_SPEED,DT,MUSKIN,TS_FOUNDi,DWARM_,TBAR_,TXW,TYW,USTARW_,   &
                      DCOOL_,TDROP_,SWCOOL_,QCOOL_,BCOOL_,LCOOL_,TDEL_,SWWARM_,QWARM_,ZETA_W_,                 &
-                     PHIW_,LANGM_,TAUTW_,uStokes_,TS(:,WATER),TWMTS,TW,FRWATER,n_iter_cool,                   &
+                     PHIW_,LANGM_,TAUTW_,uStokes_,TS(:,WATER),TWMTS,TWMTF,DELTC,TW,FRWATER,n_iter_cool,       &
                      fr_ice_thresh, epsilon_d, trim(DO_GRAD_DECAY_warmLayer))
 
 ! Copies for export
