c2d741af1fb115c88c21fd8f56f7400f25590506
gmao-jkolassa <jana.kolassa@nasa.gov>
2020-10-27 14:42:01 -0400

    added modifications from PR#349; added parameter collection for Catchment-CN.4.5 simulations

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Shared/SurfParams.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Shared/SurfParams.F90
@@ -1,10 +1,13 @@
+#include "MAPL_Generic.h"
 
+!==========================================================
 module SurfParams
 
   ! Justin, 12 Apr 2018  - Created - Replaces LAND_UPD ifdefs , added SurfParams_init,
   !                        called from GEOS_LandGridCompMod, GEOS_LandiceGridCompMod
   ! jkolassa, 11 Jun 2020 - added LSM_CHOICE as input; introduced separate land parameter collections
-  !   
+  !  
+  use MAPL_ExceptionHandling 
   implicit none
 
   private  
@@ -17,28 +20,27 @@ module SurfParams
 
     
   ! Variables set by SurfParams_init: 
-  REAL, PUBLIC, SAVE :: CSOIL_2       ! J/K - heat capacity associated w/ tsurf  
-  REAL, PUBLIC, SAVE :: WEMIN         ! kg/m^2  minimum SWE in areal fraction
-  REAL, PUBLIC, SAVE :: AICEV, AICEN  ! Used in StieglitzSnow
-  REAL, PUBLIC, SAVE :: FLWALPHA      ! SRFLW multiplier with SRFLW < 0
-  REAL, PUBLIC, SAVE :: ASTRFR, STEXP ! stress parameters in energy2
-  REAL, PUBLIC, SAVE :: RSWILT        ! parameters in rsurfp
+  REAL, PUBLIC, SAVE :: CSOIL_2      		 ! J/K - heat capacity associated w/ tsurf  
+  REAL, PUBLIC, SAVE :: WEMIN        		 ! kg/m^2  minimum SWE in areal fraction
+  REAL, PUBLIC, SAVE :: AICEV, AICEN 		 ! Used in StieglitzSnow
+  REAL, PUBLIC, SAVE :: FLWALPHA     		 ! SRFLW multiplier with SRFLW < 0
+  REAL, PUBLIC, SAVE :: ASTRFR, STEXP		 ! stress parameters in energy2
+  REAL, PUBLIC, SAVE :: RSWILT       		 ! parameters in rsurfp
 
-  LOGICAL, PUBLIC, SAVE :: LAND_FIX   ! Used for fixes and init changes that
-  	   	   	   	      ! are still default in Icarus GCM
-  LOGICAL, PUBLIC, SAVE :: LAND_FIX_CN! Similar use to LAND_FIX, but controls slightly 
-                                      ! different code blocks in catchmentCN.F90
+  LOGICAL, PUBLIC, SAVE :: LAND_FIX  		 ! Used for fixes and init changes that
+  	   	   	   	     		 ! are still default in Icarus GCM
 
   contains
   
   ! Call to get "constants" that really are variables changeable during land/landice initialization
 
-  subroutine SurfParams_init(LAND_PARAMS,LSM_CHOICE) 
+  subroutine SurfParams_init(LAND_PARAMS,LSM_CHOICE, rc) 
 
     implicit none
     
     CHARACTER(*), INTENT(IN) :: LAND_PARAMS
     INTEGER, INTENT(IN)      :: LSM_CHOICE 
+    INTEGER, OPTIONAL, INTENT(OUT) :: rc
 
     LOGICAL, SAVE :: init_called = .FALSE. ! Flag if SurfParams_init has been called
 
@@ -86,83 +88,44 @@ module SurfParams
          	RSWILT   = 500.   ! reverted
 
       	case DEFAULT                                                                            
-         	write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
-         	return                                                                               
+         	_ASSERT(.FALSE.,'LAND_PARAMS not valid or incompatible with LSM_CHOICE')                                                                            
       	end select                                                                                
                                                                                               
     else if (LSM_CHOICE==2) then       
 
 	select case (LAND_PARAMS)                                                                
        	case ("CN_CLM4")  ! parameters to reproduce Fanwei Zeng's Catchment-CN.4.0 runs (e0004s_transientCO2_05) done with build /gpfsm/dnb31/fzeng/LDASsa_m3-16_0_p2_CatchCatchCN_for_MERRA3        
-         	LAND_FIX = .TRUE.                                                                    
-         	LAND_FIX_CN = .FALSE.                                                                
-         	CSOIL_2  = 70000. ! Post H5_0  
-         	WEMIN    = 13.                                                                       
-         	AICEV    = 0.149                                                                     
-         	AICEN    = 19.851                                                                    
-         	FLWALPHA = 1.                                                                        
-         	ASTRFR   = 0.333  ! reverted 
-         	STEXP    = 1.     ! reverted                                  
-         	RSWILT   = 1500. 
-
-	case ("EL_Fire_CLM45")  ! parameters to reproduce Eunjee Lee's Catchment-CN4.5 fire carbon emission simulations
+         	LAND_FIX        = .TRUE.                                                                    
+         	CSOIL_2         = 70000. ! Post H5_0  
+         	WEMIN           = 13.                                                                       
+         	AICEV           = 0.149                                                                     
+         	AICEN           = 19.851                                                                    
+         	FLWALPHA        = 1.                                                                        
+         	ASTRFR          = 0.333  ! reverted 
+         	STEXP           = 1.     ! reverted                                  
+         	RSWILT          = 1500. 
+  
+	case ("CN_CLM45")  ! parameters to reproduce Eunjee Lee's Catchment-CN4.5 fire carbon emission simulations
                 LAND_FIX = .TRUE.
-                LAND_FIX_CN = .FALSE.
                 CSOIL_2  = 70000. ! Post H5_0  
                 WEMIN    = 13.
                 AICEV    = 0.107
                 AICEN    = 19.893
+                FLWALPHA = 0.005.
                 FLWALPHA = 0.005
                 ASTRFR   = 0.333  ! reverted 
                 STEXP    = 1.     ! reverted                                  
                 RSWILT   = 2000.  
 
-       	case ("Icarus")  ! "Old" LDASsa physics, current default for Icarus GCM                
-         	LAND_FIX = .FALSE.                                                                   
-         	LAND_FIX_CN = .TRUE.                                                                
-         	CSOIL_2  = 200.                                                                      
-         	WEMIN    = 26.                                                                       
-         	AICEV    = 0.149                                                                     
-         	AICEN    = 19.851                                                                    
-         	FLWALPHA = 0.005      ! set to previously hardcoded CatchCN value                    
-         	ASTRFR   = 0.333                                                                     
-         	STEXP    = 1.                                                                        
-         	RSWILT   = 2000.      ! set to previously hardcoded CatchCN value                    
-                                                                                              
-      	case ("V24_C05")  ! V24_C05 changes, default for LDAS m4-17-0                           
-         	LAND_FIX = .TRUE.                                                                    
-         	LAND_FIX_CN = .TRUE.                                                                 
-         	CSOIL_2  = 70000.  ! Post H5_0                                                       
-         	WEMIN    = 13.                                                                       
-         	AICEV    = 0.107                                                                     
-         	AICEN    = 19.893
-         	FLWALPHA = 0.005   ! set to previously hardcoded CatchCN value
-         	ASTRFR   = 1.                                                                        
-         	STEXP    = 2.                                                                        
-         	RSWILT   = 2000.   ! set to previously hardcoded CatchCN value   
-
-      	case ("NRv7.2")  ! SMAP NRv7.2 changes, default for after LDAS m4-17-6                  
-         	LAND_FIX = .TRUE.                                                                    
-         	LAND_FIX_CN = .TRUE.                                                                 
-         	CSOIL_2  = 70000. ! Post H5_0                                                        
-         	WEMIN    = 13.                                                                       
-         	AICEV    = 0.149
-         	AICEN    = 19.851                                                                    
-         	FLWALPHA = 0.005  ! set to previously hardcoded CatchCN value                           
-         	ASTRFR   = 0.333  ! reverted  
-         	STEXP    = 1.     ! reverted     
-         	RSWILT   = 2000.  ! set to previously hardcoded CatchCN value                        
-                                                                                              
       	case DEFAULT                                                                            
-         	write (*,*) "LAND_PARAMS not valid or incompatible with LSM_CHOICE ",LAND_PARAMS     
-         	return                                                                               
+         	_ASSERT(.FALSE.,'LAND_PARAMS not valid or incompatible with LSM_CHOICE')           
       	end select     
     else                                                                                      
-      	write (*,*) "invalid land model choice ",LSM_CHOICE                                                   
-     	 return                                                                               
+     	_ASSERT(.FALSE.,'land model choice not valid')                                                                               
     end if ! LSM_CHOICE  
 
     init_called = .TRUE.
+    _RETURN(_SUCCESS)
 
   end subroutine SurfParams_init
 
