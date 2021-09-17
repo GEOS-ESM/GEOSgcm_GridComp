89148ec61adbe6e4e1fa9e1d79e9846528ca43bf
William Putman <William.M.Putman@nasa.gov>
2021-09-17 09:08:53 -0400

    Including Peters radiation speed updates

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSradiation_GridComp/GEOSirrad_GridComp/RRTMG/rrtmg_lw/gcm_model/modules/rrlw_kg05.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSradiation_GridComp/GEOSirrad_GridComp/RRTMG/rrtmg_lw/gcm_model/modules/rrlw_kg05.F90
@@ -1,12 +1,7 @@
-#include "_gpudef.inc"
-#include "_memdef.inc" 
- module rrlw_kg05
+module rrlw_kg05
 
-       !use parkind ,only : im => kind , rb => kind 
-
-      use memory
- implicit none
-      save
+   implicit none
+   save
 
 !-----------------------------------------------------------------
 ! rrtmg_lw ORIGINAL abs. coefficients for interval 5
@@ -16,41 +11,15 @@
 ! Revised: MJIacono, AER, jun2006
 ! Revised: MJIacono, AER, aug2008
 !-----------------------------------------------------------------
-!
-!  name     type     purpose
-!  ----   : ----   : ---------------------------------------------
-!fracrefao: real    
-!fracrefbo: real
-! kao     : real     
-! kbo     : real     
-! kao_mo3 : real     
-! selfrefo: real     
-! forrefo : real     
-! ccl4o   : real
-!-----------------------------------------------------------------
 
-      integer , parameter :: no5  = 16
-      integer , parameter :: ng5  = 16
-      real  _cpusnp :: ka(9,5,13,ng5),kb(5,5,13:59,ng5)  
-      real  _cpus :: kao(9,5,13,no5)
-      real  _cpus :: kbo(5,5,13:59,no5)
+   integer, parameter :: no5 = 16
 
-      real  _cpus :: fracrefao(no5,9) ,fracrefbo(no5,5) 
-      real  _cpusnp :: absa(585,ng5)
- 
-      real  _cpus :: kao_mo3(9,19,no5)
-      real  _cpus :: selfrefo(10,no5)
-      real  _cpus :: forrefo(4,no5)
-      real  _cpus :: ccl4o(no5)
+   real :: fracrefao(no5,9), fracrefbo(no5,5) 
+   real :: kao(9,5,13,no5), kbo(5,5,13:59,no5)
+   real :: kao_mo3(9,19,no5)
+   real :: selfrefo(10,no5), forrefo(4,no5)
+   real :: ccl4o(no5)
 
-
-      real  _gpudeva :: fracrefaod(:,:) ,fracrefbod(:,:)
-      real  _gpudev :: kaod(9,5,13,no5)
-      real  _gpudev :: kbod(5,5,13:59,no5)
-      real  _gpudeva :: kao_mo3d(:,:,:)
-      real  _gpudeva :: selfrefod(:,:)
-      real  _gpudeva :: forrefod(:,:)
-      real  _gpudeva :: ccl4od(:)
 !-----------------------------------------------------------------
 ! rrtmg_lw COMBINED abs. coefficients for interval 5
 ! band 5:  700-820 cm-1 (low - h2o,co2; high - o3,co2)
@@ -59,90 +28,16 @@
 ! Revised: MJIacono, AER, jun2006
 ! Revised: MJIacono, AER, aug2008
 !-----------------------------------------------------------------
-!
-!  name     type     purpose
-!  ----   : ----   : ---------------------------------------------
-!fracrefa : real    
-!fracrefb : real
-! ka      : real     
-! kb      : real     
-! ka_mo3  : real     
-! selfref : real     
-! forref  : real     
-! ccl4    : real
-!
-! absa    : real
-! absb    : real
-!-----------------------------------------------------------------
-
-
  
-      real  _cpusnp :: absb(1175,ng5)
+   integer, parameter :: ng5 = 16
 
-      real  _cpus :: fracrefa(ng5,9) ,fracrefb(ng5,5)
-      
-      real  _cpus :: ka_mo3(9,19,ng5)
-      real  _cpus :: selfref(10,ng5)
-      real  _cpus :: forref(4,ng5)
-      real  _cpus :: ccl4(ng5)
+   real :: ka(9,5,13,   ng5), absa( 585,ng5)
+   real :: kb(5,5,13:59,ng5), absb(1175,ng5)
+   equivalence (ka(1,1,1,1),absa(1,1)), (kb(1,1,13,1),absb(1,1))
 
-      real  _gpudeva :: fracrefad(:,:) ,fracrefbd(:,:)
-      real  _gpudevanp ::  absad(:,:)
-      real  _gpudevanp ::  absbd(:,:)
-      real  _gpudeva :: ka_mo3d(:,:,:)
-      real  _gpudeva :: selfrefd(:,:)
-      real  _gpudeva :: forrefd(:,:)
-      real  _gpudeva :: ccl4d(:)
-      
-      equivalence (ka(1,1,1,1),absa(1,1)),(kb(1,1,13,1),absb(1,1))
+   real :: fracrefa(ng5,9), fracrefb(ng5,5)
+   real :: ka_mo3(9,19,ng5)
+   real :: selfref(10,ng5), forref(4,ng5)
+   real :: ccl4(ng5)
 
-      contains 
-
-      subroutine copyToGPU5
-
-       dbcop( fracrefao , fracrefaod )
-       dbcop( fracrefbo , fracrefbod )
-    
-       dbcop( kao_mo3 , kao_mo3d )
-       dbcop( selfrefo , selfrefod )
-       dbcop( forrefo , forrefod )
-       dbcop( ccl4o , ccl4od )
-
-       dbcop( fracrefa , fracrefad )
-       dbcop( fracrefb , fracrefbd )
-
-       dbcopnp( absa , absad, 585, ng5 )
-       dbcopnp( absb , absbd, 1175, ng5 )
-
-       dbcop( ka_mo3 , ka_mo3d )
-       dbcop( selfref , selfrefd )
-       dbcop( forref , forrefd )
-       dbcop( ccl4 , ccl4d )
-
-      end subroutine 
-
-      subroutine reg5
-    
-       dbreg( fracrefao )
-       dbreg( fracrefbo )
-     
-       dbreg( kao_mo3 )
-       dbreg( selfrefo )
-       dbreg( forrefo )
-       dbreg( ccl4o )
-
-       dbreg( fracrefa )
-       dbreg( fracrefb )
-      
-       dbreg( absa )
-     
-       dbreg( absb )
-       dbreg( ka_mo3 )
-       dbreg( selfref )
-       dbreg( forref )
-       dbreg( ccl4 )
-
-      end subroutine 
-
-      end module rrlw_kg05
-
+end module rrlw_kg05
