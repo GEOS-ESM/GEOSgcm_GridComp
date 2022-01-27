b3878281e1d5df3428c9d9c73643cf1edcddca1d
Rolf Reichle <rolf.reichle@nasa.gov>
2022-01-26 22:21:13 -0500

    fixing typo in previous commit (PEATCLSM poros threshold)

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/Shared/catch_constants.f90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/Shared/catch_constants.f90
@@ -90,7 +90,7 @@ module catch_constants
   !
   ! - reichle, 26 Jan 2022
   
-  REAL, PARAMETER, PUBLIC :: PEATCLSM_THRESHOLD_POROS  = 0.90    ! [m3/m3]
+  REAL, PARAMETER, PUBLIC :: PEATCLSM_POROS_THRESHOLD  = 0.90    ! [m3/m3]
 
   ! max zbar for specific yield calc in PEATCLSM
   
