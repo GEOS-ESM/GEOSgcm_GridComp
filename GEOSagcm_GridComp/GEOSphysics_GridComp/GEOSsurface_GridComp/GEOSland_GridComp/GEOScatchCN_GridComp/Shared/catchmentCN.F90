c838042b6e0e0d96be6bee66beeaa8c88557cfc9
gmao-jkolassa <jana.kolassa@nasa.gov>
2022-02-09 14:55:10 -0500

    updating call to gndtmp to reflect new order or input arguments

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/Shared/catchmentCN.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/Shared/catchmentCN.F90
@@ -1062,9 +1062,9 @@ CONTAINS
         FH21=-GHFLUX(N)
 
         CALL GNDTMP(                                                           &
-              DTS=dtstep,phi,zbar,THETAF=thetaf,FH21=fh21,                     &
+              phi,zbar,                                                        &
               ht,                                                              &
-              xfice,tp, soilice)
+              xfice,tp, soilice,DTS=dtstep,THETAF=thetaf,FH21=fh21)
 
         DO LAYER=1,6
           GHTCNT(LAYER,N)=HT(LAYER)
