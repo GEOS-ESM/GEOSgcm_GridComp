69ca6d7fb4948c49b6e3663f2c14c29c6d221f08
William Putman <William.M.Putman@nasa.gov>
2022-03-29 22:31:42 -0400

    updated use

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/GEOS_GFDL_1M_InterfaceMod.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/GEOS_GFDL_1M_InterfaceMod.F90
@@ -12,7 +12,7 @@ module GEOS_GFDL_1M_InterfaceMod
 
   use ESMF
   use MAPL
-  use aer_cloud, only: AerProps
+  use aer_cloud
   use Aer_Actv_Single_Moment, only: Aer_Actv_1M_interface, USE_AEROSOL_NN, R_AIR
   use gfdl2_cloud_microphys_mod
 
