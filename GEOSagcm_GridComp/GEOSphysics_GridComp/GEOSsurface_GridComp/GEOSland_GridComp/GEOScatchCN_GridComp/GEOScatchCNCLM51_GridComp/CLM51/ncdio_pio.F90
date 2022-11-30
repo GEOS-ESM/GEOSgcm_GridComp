eb70021d5bfc0bda5d08e2591ee564a01d68c0cd
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2022-11-30 09:46:31 -0500

    add reading scalar

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/ncdio_pio.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/ncdio_pio.F90
@@ -36,14 +36,17 @@ module ncdio_pio
 
   interface ncd_io
 
+    module procedure ncd_io_r4_0d
     module procedure ncd_io_r4_1d
     module procedure ncd_io_r4_2d
     module procedure ncd_io_r4_3d
     module procedure ncd_io_r4_4d
+    module procedure ncd_io_r8_0d
     module procedure ncd_io_r8_1d
     module procedure ncd_io_r8_2d
     module procedure ncd_io_r8_3d
     module procedure ncd_io_r8_4d
+    module procedure ncd_io_i4_0d
     module procedure ncd_io_i4_1d
     module procedure ncd_io_i4_2d
     module procedure ncd_io_i4_3d
@@ -52,6 +55,33 @@ module ncdio_pio
   end interface
 
  contains 
+
+!----------------------------------------------------
+ subroutine ncd_io_r4_0d ( varname, data, flag, ncid, readvar, rc)
+
+ ! ARGUMENTS:
+ !-------------
+  type(file_desc_t), intent(inout) :: ncid         ! netcdf file id
+  real(r4),          intent(inout) :: data
+  character(len=*),  intent(in)    :: flag         ! 'read' or 'write'
+  character(len=*),  intent(in)    :: varname      ! variable name
+  logical,           intent(out)   :: readvar
+  integer,optional,  intent(out)   :: rc
+
+  ! LOCAL:
+
+  integer :: status
+
+  !-------------------------------------
+
+   if (flag == 'read') then
+      readvar = .false.
+     ! call ncid%get_var(varname, data, rc=status)
+      call MAPL_VarRead(ncid,varname,data,status)
+      if (status ==0) readvar = .true.
+   endif
+
+ end subroutine ncd_io_r4_0d
 
 !----------------------------------------------------
  subroutine ncd_io_r4_1d ( varname, data, flag, ncid, readvar, rc)
@@ -163,7 +193,35 @@ module ncdio_pio
 
  end subroutine ncd_io_r4_4d
 
-  !-----------------------------------------------------------------------
+ !-----------------------------------------------------------------------
+
+ subroutine ncd_io_r8_0d ( varname, data, flag, ncid, readvar, rc)
+
+ ! ARGUMENTS:
+ !-------------
+  type(file_desc_t), intent(inout) :: ncid         ! netcdf file id
+  real(r8),          intent(inout) :: data
+  character(len=*),  intent(in)    :: flag         ! 'read' or 'write'
+  character(len=*),  intent(in)    :: varname      ! variable name
+  logical,           intent(out)   :: readvar
+  integer, optional, intent(out)   :: rc
+
+  ! LOCAL:
+
+  integer :: status
+
+  !-------------------------------------
+
+   if (flag == 'read') then
+      readvar = .false.
+      !call ncid%get_var(varname, data, rc=status)
+      call MAPL_VarRead(ncid,varname,data,status)
+      if (status ==0) readvar = .true.
+   endif
+
+ end subroutine ncd_io_r8_0d
+
+ !-----------------------------------------------------------------------
 
  subroutine ncd_io_r8_1d ( varname, data, flag, ncid, readvar, rc)
 
@@ -191,8 +249,7 @@ module ncdio_pio
 
  end subroutine ncd_io_r8_1d
 
-  !-----------------------------------------------------------------------
-
+ !-----------------------------------------------------------------------
 
  subroutine ncd_io_r8_2d ( varname, data, flag, ncid, readvar, rc)
 
@@ -278,7 +335,35 @@ module ncdio_pio
 
  end subroutine ncd_io_r8_4d
 
-  !-----------------------------------------------------------------------
+ !-----------------------------------------------------------------------
+ subroutine ncd_io_i4_0d ( varname, data, flag, ncid, readvar, rc)
+
+ ! ARGUMENTS:
+ !-------------
+  type(file_desc_t), intent(inout) :: ncid         ! netcdf file id
+  integer(i4),       intent(inout) :: data
+  character(len=*),  intent(in)    :: flag         ! 'read' or 'write'
+  character(len=*),  intent(in)    :: varname      ! variable name
+  logical,           intent(out)   :: readvar
+  integer, optional, intent(out)   :: rc
+
+  ! LOCAL:
+
+  integer :: status
+
+  !-------------------------------------
+
+   if (flag == 'read') then
+      readvar = .false.
+      !call ncid%get_var(varname, data, rc=status)
+      call MAPL_VarRead(ncid,varname,data,status)
+      if (status ==0) readvar = .true.
+   endif
+
+ end subroutine ncd_io_i4_0d
+
+ !-----------------------------------------------------------------------
+
  subroutine ncd_io_i4_1d ( varname, data, flag, ncid, readvar, rc)
 
  ! ARGUMENTS:
@@ -305,7 +390,8 @@ module ncdio_pio
 
  end subroutine ncd_io_i4_1d
 
-  !-----------------------------------------------------------------------
+ !-----------------------------------------------------------------------
+
  subroutine ncd_io_i4_2d ( varname, data, flag, ncid, readvar, rc)
 
  ! ARGUMENTS:
