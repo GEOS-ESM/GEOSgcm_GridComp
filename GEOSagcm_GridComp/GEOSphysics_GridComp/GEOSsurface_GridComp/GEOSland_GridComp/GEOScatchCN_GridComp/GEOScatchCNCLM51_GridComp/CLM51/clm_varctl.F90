938c962250dc093418947300e9b2e41e7a134765
gmao-jkolassa <jana.kolassa@nasa.gov>
2022-12-05 13:53:28 -0500

    add missing variable

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/clm_varctl.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/clm_varctl.F90
@@ -36,6 +36,7 @@ module clm_varctl
   logical, public :: use_dynroot         = .false.
   logical, public :: use_bedrock = .false. ! true => use spatially variable soil depth
   logical, public :: use_extralakelayers = .false.
+  logical, public :: use_biomass_heat_storage = .false.
 
   logical, public :: use_c13 = .false.                  ! true => use C-13 model
   logical, public :: use_c14 = .false.                  ! true => use C-14 model
