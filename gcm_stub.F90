13a7dede840c37e99edf407d20ba0c5193509020
Benjamin Auer <benjamin.m.auer@nasa.gov>
2022-12-08 16:52:30 -0500

    brach for gcm/modelE work

---
+++ gcm_stub.F90
@@ -0,0 +1,12 @@
+module gcm_stub_for_modelE
+implicit none
+private
+
+public gcm_stub_routine
+
+contains
+  
+   subroutine gcm_stub_routine()
+      print*,"hello world"
+   end subroutine
+end module
