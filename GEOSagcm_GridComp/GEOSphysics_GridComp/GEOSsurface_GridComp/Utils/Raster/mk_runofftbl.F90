b816c9558533536ccda1867b588e63f681cc695a
sanAkel <santha.akella@nasa.gov>
2022-12-25 16:12:17 -0500

    remove f2py commented lines in subroutine reroute

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/mk_runofftbl.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/mk_runofftbl.F90
@@ -329,21 +329,18 @@ contains
              lons, lats, area, type, tnum, Nr, Nt, Nto, im, jm)
   implicit none
   integer, intent(inout) :: dstind(Nr)
-  real, intent(inout) :: weight(Nr) ! New destination index in routing table
-  real, intent(in)  :: slmask(jm,im) ! sea-land mask
-  integer, intent(in) :: type(Nt) ! input tile type (ocean=0 or lake=19)
-  integer, intent(in)  :: ii(Nt), jj(Nt) ! indexes of ocean grid
-  real, intent(in)  :: lons(Nt), lats(Nt), area(Nt)  ! lons and lats and area of tiles
-  integer, intent(in) :: tnum(Nto) ! number of ocean tile in tile file
-  integer, intent(in) :: Nr, Nt, Nto, im, jm
+  real,    intent(inout) :: weight(Nr)     ! New destination index in routing table
+  real,    intent(in)    :: slmask(jm,im)  ! sea-land mask
+  integer, intent(in)    :: type(Nt)       ! input tile type (ocean=0 or lake=19)
+  integer, intent(in)    :: ii(Nt), jj(Nt) ! indexes of ocean grid
+  real,    intent(in)    :: lons(Nt), lats(Nt), area(Nt)  ! lons and lats and area of tiles
+  integer, intent(in)    :: tnum(Nto)      ! number of ocean tile in tile file
+  integer, intent(in)    :: Nr, Nt, Nto, im, jm
+
   real :: circles(3,Nt) ! metrics of great circle
-  real :: dist(Nto) ! distances to re-routed tile
+  real :: dist(Nto)     ! distances to re-routed tile
   real :: deg2rad
   integer :: i, dind, tt, nmoved
-
-  !f2py intent(in) :: slmask, ii, jj, lons, lats, area, type, tnum
-  !f2py intent(in,out) :: dstind, weight
-  !f2py intent(hide) :: Nr, Nt, Nto, im, jm
 
   deg2rad=acos(-1.)/180.
   circles(1,:)=sin(lats*deg2rad)
