d9984b479b4d1b172208a1acf8820ee064dd42a3
gmao-jkolassa <jana.kolassa@nasa.gov>
2023-01-26 15:36:17 -0500

    add function declaration

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/CN_init_mod.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/CN_init_mod.F90
@@ -6,7 +6,7 @@ module CN_initMod
 
   use clm_varcon        , only : clm_varcon_init
   use clm_varpar        , only : VAR_COL, VAR_PFT, clm_varpar_init
-  use clm_varctl        , only : use_century_decomp
+  use clm_varctl        , only : use_century_decomp, init_clm_varctl
   use decompMod
   use filterMod
   use CNVegNitrogenStateType
