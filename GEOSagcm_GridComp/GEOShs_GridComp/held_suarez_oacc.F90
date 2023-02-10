15defe0467f776dfcb3e0abbdddba2ddf8fa2e3c
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2023-02-10 15:01:42 -0500

    Removed some trailing whitespace

--- GEOSagcm_GridComp/GEOShs_GridComp/held_suarez_oacc.F90
+++ GEOSagcm_GridComp/GEOShs_GridComp/held_suarez_oacc.F90
@@ -54,14 +54,14 @@ contains
     DP_s = 0.0
     PL_s = 0.0
     UU_s = 0.0
-    VV_s = 0.0 
-    VR_s = 0.0 
-    TE_s = 0.0 
-    DS_s = 0.0 
-    PII_s = 0.0 
-    F1_s= 0.0 
-    RR_s = 0.0 
-    DM_s = 0.0 
+    VV_s = 0.0
+    VR_s = 0.0
+    TE_s = 0.0
+    DS_s = 0.0
+    PII_s = 0.0
+    F1_s= 0.0
+    RR_s = 0.0
+    DM_s = 0.0
     PK_s = 0.0
     t1 = 0.0
     t2 = 0.0
@@ -109,123 +109,123 @@ contains
     !$acc      copy(DTDT, DUDT, DVDT, T, T_EQ, THEQ, U, V) &
     !$acc      copyin(PS, PT)
 
-       ! write(*,*) 'Running HS Code with outer loop OpenMP/OpenACC Parallelism'
-       call cpu_time(t1)
+    ! write(*,*) 'Running HS Code with outer loop OpenMP/OpenACC Parallelism'
+    call cpu_time(t1)
 
-       !$acc parallel loop collapse(3) present(CPHI2, HFCN, P_I, SPHI2, PLE) &
-       !$acc                           present(DISS, TAUX, TAUY) &
-       !$acc                           present(DTDT, DUDT, DVDT, T, T_EQ, THEQ, U, V) &
-       !$acc                           present(PS, PT) &
-       !$acc                           private(DP_s, PL_s, UU_s, VV_s, VR_s, TE_s, DS_s, PII_s, F1_s, RR_s, DM_s, PK_s)
-       do L = 1,LM
-          do J = 1, JM
-             do I = 1, IM
+    !$acc parallel loop collapse(3) present(CPHI2, HFCN, P_I, SPHI2, PLE) &
+    !$acc                           present(DISS, TAUX, TAUY) &
+    !$acc                           present(DTDT, DUDT, DVDT, T, T_EQ, THEQ, U, V) &
+    !$acc                           present(PS, PT) &
+    !$acc                           private(DP_s, PL_s, UU_s, VV_s, VR_s, TE_s, DS_s, PII_s, F1_s, RR_s, DM_s, PK_s)
+    do L = 1,LM
+       do J = 1, JM
+          do I = 1, IM
 
-                ! If running with OpenMP, use these assignments for DP and PL
-                DP_s  = (PLE(I,J,L)-PLE(I,J,L-1))
-                PL_s  = (PLE(I,J,L)+PLE(I,J,L-1))*0.5
+             ! If running with OpenMP, use these assignments for DP and PL
+             DP_s  = (PLE(I,J,L)-PLE(I,J,L-1))
+             PL_s  = (PLE(I,J,L)+PLE(I,J,L-1))*0.5
 
-                ! If running with OpenACC, use these assignments for DP and PL
-                !DP_s  = (PLE(I,J,L+1)-PLE(I,J,L))
-                !PL_s  = (PLE(I,J,L+1)+PLE(I,J,L))*0.5
-                DM_s  = DP_s / GRAV
-                PK_s  = (PL_s/P00)**KAPPA 
+             ! If running with OpenACC, use these assignments for DP and PL
+             !DP_s  = (PLE(I,J,L+1)-PLE(I,J,L))
+             !PL_s  = (PLE(I,J,L+1)+PLE(I,J,L))*0.5
+             DM_s  = DP_s / GRAV
+             PK_s  = (PL_s/P00)**KAPPA
 
-                ! H&S equilibrium temperature
-                !----------------------------
+             ! H&S equilibrium temperature
+             !----------------------------
 
-                TE_s  = PK_s*( T0 - DELH*SPHI2(I,J) - DELV1*CPHI2(I,J)*log( PL_s/P00 ) )
-                TE_s  = max( TE_s, TSTRT )
+             TE_s  = PK_s*( T0 - DELH*SPHI2(I,J) - DELV1*CPHI2(I,J)*log( PL_s/P00 ) )
+             TE_s  = max( TE_s, TSTRT )
 
-                ! Williamson Stratospheric modifications to equilibrium temperature
-                ! -----------------------------------------------------------------
+             ! Williamson Stratospheric modifications to equilibrium temperature
+             ! -----------------------------------------------------------------
 
-                if( PL_s < P_D ) then
-                   TE_s  = TSTRT*( min(1.0,PL_s/P_D)**(RGAS*GAM_D/GRAV)     &
-                        + min(1.0,PL_s/PII_s)**(RGAS*GAM_I/GRAV) - 1 )
+             if( PL_s < P_D ) then
+                TE_s  = TSTRT*( min(1.0,PL_s/P_D)**(RGAS*GAM_D/GRAV)     &
+                     + min(1.0,PL_s/PII_s)**(RGAS*GAM_I/GRAV) - 1 )
+             endif
+
+             !  Exports of equilibrium T and Theta
+             !------------------------------------
+
+             if(associated(T_EQ)) T_EQ(I,J,L) = TE_s
+             ! T_EQ(I,J,L) = TE_s
+             if(associated(THEQ)) THEQ(I,J,L) = TE_s/PK_s
+             ! THEQ(I,J,L) = TE_s/PK_s
+
+             ! Vertical structure of timescales in H&S.
+             !---------------------------------------------
+
+             F1_s  = max(0.0, ( (PL_s/PS(I,J))-SIG1 )/( 1.0-SIG1 ) )
+
+             ! Atmospheric heating from H&S
+             !-----------------------------
+
+             RR_s = (KA + (KS-KA)*F1_s*CPHI2(I,J)**2) * (TE_s-T(I,J,L))
+
+             if(associated(DTDT)) DTDT(I,J,L) = DP_s*RR_s
+             ! DTDT(I,J,L) = DP_s*RR_s
+             if(FriendlyTemp    ) T   (I,J,L) = T(I,J,L) + DT*RR_s
+
+             ! Wind tendencies
+             !----------------
+
+             UU_s  = -U(I,J,L)*(F1_s*KF)
+             VV_s  = -V(I,J,L)*(F1_s*KF)
+
+             if(associated(DUDT)) DUDT(I,J,L) = UU_s
+             ! DUDT(I,J,L) = UU_s
+             if(associated(DVDT)) DVDT(I,J,L) = VV_s
+             ! DVDT(I,J,L) = VV_s
+
+             if(FriendlyWind) then
+                U(I,J,L) = U(I,J,L) + DT*UU_s
+                V(I,J,L) = V(I,J,L) + DT*VV_s
+             end if
+
+             !  Frictional heating from H&S drag
+             !----------------------------------
+
+             DS_s = U(I,J,L)*UU_s + V(I,J,L)*VV_s
+
+             if(associated(DISS)) DISS(I,J) = DISS(I,J) - DS_s*DM_s
+             ! DISS(I,J) = DISS(I,J) - DS_s*DM_s
+             if(FRICQ /= 0) then
+                if(associated(DTDT)) DTDT(I,J,L) = DTDT(I,J,L) - DS_s*(DP_s/CP  )
+                ! DTDT(I,J,L) = DTDT(I,J,L) - DS_s*(DP_s/CP  )
+                if(FriendlYTemp    ) T   (I,J,L) = T   (I,J,L) - DS_s*(DT/CP  )
+             end if
+
+             !  Surface stresses from vertically integrated H&S surface drag
+             !--------------------------------------------------------------
+
+             if(associated(TAUX)) TAUX(I,J) = TAUX(I,J) - UU_s*DM_s
+             ! TAUX(I,J) = TAUX(I,J) - UU_s*DM_s
+             if(associated(TAUY)) TAUY(I,J) = TAUY(I,J) - VV_s*DM_s
+             ! TAUY(I,J) = TAUY(I,J) - VV_s*DM_s
+
+             ! Localized heat source, if any
+             !------------------------------
+
+             if((associated(DTDT).or.FriendlyTemp) .and. QMAX/=0.0) then
+                ! if(FriendlyTemp .and. QMAX/=0.0) then
+                if(PL_s > P_1) then
+                   VR_s = HFCN(I,J)*(QMAX/DAYLEN)*sin( PI*(PS(I,J)-PL_s)/(PS(I,J)-P_1) )
+                else
+                   VR_s = 0.
                 endif
 
-                !  Exports of equilibrium T and Theta
-                !------------------------------------
-
-                if(associated(T_EQ)) T_EQ(I,J,L) = TE_s
-                ! T_EQ(I,J,L) = TE_s
-                if(associated(THEQ)) THEQ(I,J,L) = TE_s/PK_s
-                ! THEQ(I,J,L) = TE_s/PK_s
-
-                ! Vertical structure of timescales in H&S.
-                !---------------------------------------------
-
-                F1_s  = max(0.0, ( (PL_s/PS(I,J))-SIG1 )/( 1.0-SIG1 ) )
-
-                ! Atmospheric heating from H&S
-                !-----------------------------
-
-                RR_s = (KA + (KS-KA)*F1_s*CPHI2(I,J)**2) * (TE_s-T(I,J,L))
-
-                if(associated(DTDT)) DTDT(I,J,L) = DP_s*RR_s
-                ! DTDT(I,J,L) = DP_s*RR_s
-                if(FriendlyTemp    ) T   (I,J,L) = T(I,J,L) + DT*RR_s
-
-                ! Wind tendencies
-                !----------------
-
-                UU_s  = -U(I,J,L)*(F1_s*KF)
-                VV_s  = -V(I,J,L)*(F1_s*KF)
-
-                if(associated(DUDT)) DUDT(I,J,L) = UU_s
-                ! DUDT(I,J,L) = UU_s
-                if(associated(DVDT)) DVDT(I,J,L) = VV_s
-                ! DVDT(I,J,L) = VV_s
-
-                if(FriendlyWind) then
-                   U(I,J,L) = U(I,J,L) + DT*UU_s
-                   V(I,J,L) = V(I,J,L) + DT*VV_s
-                end if
-
-                !  Frictional heating from H&S drag
-                !----------------------------------
-
-                DS_s = U(I,J,L)*UU_s + V(I,J,L)*VV_s
-
-                if(associated(DISS)) DISS(I,J) = DISS(I,J) - DS_s*DM_s
-                ! DISS(I,J) = DISS(I,J) - DS_s*DM_s
-                if(FRICQ /= 0) then
-                   if(associated(DTDT)) DTDT(I,J,L) = DTDT(I,J,L) - DS_s*(DP_s/CP  )
-                   ! DTDT(I,J,L) = DTDT(I,J,L) - DS_s*(DP_s/CP  )
-                   if(FriendlYTemp    ) T   (I,J,L) = T   (I,J,L) - DS_s*(DT/CP  )
-                end if
-
-                !  Surface stresses from vertically integrated H&S surface drag
-                !--------------------------------------------------------------
-
-                if(associated(TAUX)) TAUX(I,J) = TAUX(I,J) - UU_s*DM_s
-                ! TAUX(I,J) = TAUX(I,J) - UU_s*DM_s
-                if(associated(TAUY)) TAUY(I,J) = TAUY(I,J) - VV_s*DM_s
-                ! TAUY(I,J) = TAUY(I,J) - VV_s*DM_s
-
-                ! Localized heat source, if any
-                !------------------------------
-
-                if((associated(DTDT).or.FriendlyTemp) .and. QMAX/=0.0) then
-                   ! if(FriendlyTemp .and. QMAX/=0.0) then
-                   if(PL_s > P_1) then
-                      VR_s = HFCN(I,J)*(QMAX/DAYLEN)*sin( PI*(PS(I,J)-PL_s)/(PS(I,J)-P_1) )
-                   else
-                      VR_s = 0.
-                   endif
-
-                   if(associated(DTDT)) DTDT(I,J,L) = DTDT(I,J,L) + DP_s*VR_s
-                   ! DTDT(I,J,L) = DTDT(I,J,L) + DP_s*VR_s
-                   if(FriendlyTemp    ) T   (I,J,L) = T   (I,J,L) + DT*VR_s
-                end if
-             enddo
+                if(associated(DTDT)) DTDT(I,J,L) = DTDT(I,J,L) + DP_s*VR_s
+                ! DTDT(I,J,L) = DTDT(I,J,L) + DP_s*VR_s
+                if(FriendlyTemp    ) T   (I,J,L) = T   (I,J,L) + DT*VR_s
+             end if
           enddo
        enddo
-       !$omp end parallel do
-       !$acc end parallel loop
-       call cpu_time(t2)
-       !!$acc update host (DTDT, DUDT, DVDT, T_EQ, T, U, V)
+    enddo
+    !$omp end parallel do
+    !$acc end parallel loop
+    call cpu_time(t2)
+    !!$acc update host (DTDT, DUDT, DVDT, T_EQ, T, U, V)
 
     !$acc end data
 
