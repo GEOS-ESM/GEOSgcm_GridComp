3ecdf141fa91a626e43fd553d6372fc3d4a09cca
biljanaorescanin <biljana.orescanin@nasa.gov>
2023-08-20 16:30:48 -0400

    fix file name

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/makebcs/make_bcs_shared.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/makebcs/make_bcs_shared.py
@@ -54,6 +54,15 @@ EOF
 sed -f sedfile       {GRIDNAME}{RS}.til > tile.file
 /bin/mv -f tile.file {GRIDNAME}{RS}.til
 /bin/rm -f sedfile
+elseif
+if( {TRIPOL_OCEAN} == True ) then
+cat > sedfile << EOF
+s/CF{NC}x6C/PE{nc}x{nc6}-CF/g
+s/{OCEAN_VERSION}-{DATENAME}{IMO}x{POLENAME}{JMO}-Pfafstetter/PE{imo}x{jmo}-{DATENAME}/g
+EOF
+sed -f sedfile       {GRIDNAME}{RS}.til > tile.file
+/bin/mv -f tile.file {GRIDNAME}{RS}.til
+/bin/rm -f sedfile
 else
 cat > sedfile << EOF
 s/CF{NC}x6C/PE{nc}x{nc6}-CF/g
