ffaf351e84acf6b3c957f153fd4b9253f9286a25
Michael Mehari <107200142+mfmehari@users.noreply.github.com>
2023-08-23 13:20:05 -0400

    Update GEOS_SurfaceGridComp.F90
    DO_DATAATM changed to DO_DATA_ATM4OCN

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOS_SurfaceGridComp.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOS_SurfaceGridComp.F90
@@ -100,7 +100,7 @@ module GEOS_SurfaceGridCompMod
   integer :: CHOOSEMOSFC 
   logical :: DO_GOSWIM
   logical :: DO_FIRE_DANGER
-  integer :: DO_DATAATM
+  integer :: DO_DATA_ATM4OCN
 
 ! used only when DO_OBIO==1 or ATM_CO2 == ATM_CO2_FOUR
   integer, parameter :: NB_CHOU_UV   = 5 ! Number of UV bands
@@ -232,7 +232,7 @@ module GEOS_SurfaceGridCompMod
 
     ! Are we running DataAtm?
     !------------------------
-    call MAPL_GetResource ( MAPL, DO_DATAATM, Label="USE_DATAATM:" , DEFAULT=0, RC=STATUS)
+    call MAPL_GetResource ( MAPL, DO_DATA_ATM4OCN, Label="USE_DATAATM:" , DEFAULT=0, RC=STATUS)
     VERIFY_(STATUS)
 
 ! Create Surface Config
@@ -653,7 +653,7 @@ module GEOS_SurfaceGridCompMod
          VLOCATION  = MAPL_VLocationNone,               RC=STATUS  )
     VERIFY_(STATUS)
 
-    if (DO_DATAATM /= 0) then
+    if (DO_DATA_ATM4OCN /= 0) then
        call MAPL_AddImportSpec ( gc,                               &
          SHORT_NAME = 'DISCHARGE',                                 &
          LONG_NAME  = 'river_discharge_at_ocean_points',           &
@@ -7096,7 +7096,7 @@ module GEOS_SurfaceGridCompMod
     call MKTILE(LWNDSRF ,LWNDSRFTILE ,NT,RC=STATUS); VERIFY_(STATUS)
     call MKTILE(SWNDSRF ,SWNDSRFTILE ,NT,RC=STATUS); VERIFY_(STATUS)
 
-    if (associated(SURF_INTERNAL_STATE%RoutingType) .or. DO_DATAATM /=0) then ! routing file exists or we run DataAtm
+    if (associated(SURF_INTERNAL_STATE%RoutingType) .or. DO_DATA_ATM4OCN /=0) then ! routing file exists or we run DataAtm
        allocate(DISCHARGETILE(NT),stat=STATUS); VERIFY_(STATUS)
        DISCHARGETILE=MAPL_Undef
        allocate(RUNOFFTILE(NT),stat=STATUS); VERIFY_(STATUS)
@@ -7247,7 +7247,7 @@ module GEOS_SurfaceGridCompMod
 
        ! Create discharge at exit tiles by routing runoff
 
-       if (DO_DATAATM /= 0) then
+       if (DO_DATA_ATM4OCN /= 0) then
           call MAPL_GetPointer(IMPORT  , DISCHARGE_IM, 'DISCHARGE',  RC=STATUS); VERIFY_(STATUS)
           call MAPL_LocStreamTransform( LOCSTREAM,  RUNOFFTILE, DISCHARGE_IM, RC=STATUS)
           VERIFY_(STATUS)
@@ -8263,7 +8263,7 @@ module GEOS_SurfaceGridCompMod
        if(associated( QSTAR)) QSTAR = (EVAP       + DEVAP*DQS)/(RHOS*FAC)
     end if
 
-    if (DO_DATAATM /= 0) then
+    if (DO_DATA_ATM4OCN /= 0) then
        ! dataAtm operates only on "saltwater" tiles. 
        ! we need to handle grid boxes withot any ocean
        ! and avoid division by 0
