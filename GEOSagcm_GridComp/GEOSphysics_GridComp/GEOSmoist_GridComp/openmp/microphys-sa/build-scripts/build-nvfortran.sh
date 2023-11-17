5607ef0db28428f667142276a9e11c9f7ac2066f
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2023-11-16 22:35:13 -0800

    Cleaned up driver routine

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/openmp/microphys-sa/build-scripts/build-nvfortran.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/openmp/microphys-sa/build-scripts/build-nvfortran.sh
@@ -5,4 +5,4 @@ nvfortran \
     -mp=gpu \
     -gpu=cc80 \
     -o microphys_driver.x \
-    gfdl_cloud_microphys.F90 gfdl_cloud_microphys_orig.F90 driver.F90
+    input.F90 gfdl_cloud_microphys.F90 gfdl_cloud_microphys_orig.F90 driver.F90
