671b8fdf5479896284625b0e88ce8c8bd8aae69e
Yujin Zeng <Yujin.Zeng@noaa.gov>
2024-01-24 15:41:55 -0500

    removing the dependence on Pfafstetter.rst

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/routing/get_landocean_Greenland_real.f90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/routing/get_landocean_Greenland_real.f90
@@ -1,36 +1,33 @@
 program main
   
-  use routing_constant, only : nc,nlon,nlat,nlon_G,nlat_G,loni_min,loni_max,lati_min,lati_max
+  use routing_constant, only : nc,nlon,nlat,nlon_G,nlat_G,loni_min,loni_max,lati_min,lati_max,&
+                               nlon1m,nlat1m
   
   implicit none
   include 'netcdf.inc'
   
   real*8,allocatable,dimension(:)    :: lon_G,lat_G
-  integer,allocatable,dimension(:,:) :: landocean,Greenland
+  integer,allocatable,dimension(:,:) :: catchind,landocean,Greenland
   integer,allocatable,dimension(:)   :: Pfaf_real, countc
   
-  integer :: i,j,ret,ncid,varid,ntile,id_glac,id_lake,id_landend
+  integer :: i,j,ret,ncid,varid,ntile
   real    :: val(4)
   
+  allocate(catchind(nlon1m,nlat1m))
+  ret=nf_open("/discover/nobackup/projects/gmao/bcs_shared/make_bcs_inputs/land/topo/v1/SRTM-TopoData/SRTM_PfafData.nc",0,ncid)
+  ret=nf_inq_varid(ncid,"CatchIndex",varid)
+  ret=nf_get_var_int(ncid,varid,catchind)
+  ret=nf_close(ncid)
   allocate(landocean(nlon,nlat))
-  open(77,file="/discover/nobackup/projects/gmao/bcs_shared/legacy_bcs/Icarus-NLv5/Icarus-NLv5_EASE/SMAP_EASEv2_M09/rst/Pfafstetter.rst",form="unformatted",status="old")
-  do j=1,nlat
-    read(77) landocean(:,j)
+  landocean=-9999
+  do i=1,nlon1m
+    do j=1,nlat1m
+      if(catchind(i,j)/=-9999)then
+        landocean(2*i-1:2*i,2*j-1:2*j)=catchind(i,j)
+      endif
+    enddo
   enddo
-  close(77)   
 
-  open(77,file="/discover/nobackup/projects/gmao/bcs_shared/legacy_bcs/Icarus-NLv5/Icarus-NLv5_EASE/SMAP_EASEv2_M09/til/Pfafstetter.til",form="formatted",status="old")
-  read(77,*) ntile
-  id_glac=ntile
-  id_lake=ntile-1
-  id_landend=ntile-3
-  allocate(Pfaf_real(id_landend))  
-  do i=1,4
-    read(77,*)
-  enddo
-  do i=1,id_landend
-    read(77,*) val(1:4),Pfaf_real(i)
-  enddo
 
   allocate(Greenland(nlon_G,nlat_G))
   allocate(lon_G(nlon_G),lat_G(nlat_G))
@@ -48,20 +45,12 @@ program main
   ret=nf_close(ncid)
   
   
-  where(Greenland/=-9999.and.(landocean(loni_min:loni_max,lati_min:lati_max)<=id_landend.or.&
-       landocean(loni_min:loni_max,lati_min:lati_max)==id_glac ))&
-       landocean(loni_min:loni_max,lati_min:lati_max)=Greenland
-  
-  
-  where(landocean>id_landend.and.landocean<id_lake) landocean=-9999
-  where(landocean==id_lake.or.landocean==id_glac) landocean=0
+  where(Greenland/=-9999) landocean(loni_min:loni_max,lati_min:lati_max)=Greenland
   
   
   do i=1,nlon
      do j=1,nlat
-        if(landocean(i,j)<=id_landend.and.landocean(i,j)>=1)then
-           landocean(i,j)=Pfaf_real(landocean(i,j))
-        else if(landocean(i,j)>=700000000)then
+        if(landocean(i,j)>=700000000)then
            landocean(i,j)=landocean(i,j)-700000000+nc
         endif
      enddo
