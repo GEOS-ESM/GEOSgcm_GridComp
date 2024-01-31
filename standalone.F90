cc8de47e03327b3d878882fdc42688020521af9b
Christopher Kung <christopher.w.kung@nasa.gov>
2024-01-31 17:24:43 -0500

    Removed specific directory references in standalone.F90

--- standalone.F90
+++ standalone.F90
@@ -17,24 +17,24 @@ program test_moist_subroutines
         call get_command_argument(2, rank_str)
     endif
 
-    if (dirName(1:10) == './c24_data') then
-        IM = 24
-        JM = 24
-        LM = 72
-    elseif (dirName(1:10) == './c90_data') then
-        IM = 90
-        JM = 90
-        LM = 72
+    ! if (dirName(1:10) == './c24_data') then
+    !     IM = 24
+    !     JM = 24
+    !     LM = 72
+    ! elseif (dirName(1:10) == './c90_data') then
+    !     IM = 90
+    !     JM = 90
+    !     LM = 72
 
-    elseif (dirName(1:11) == './c180_data') then
-        IM = 180
-        JM = 180
-        LM = 72
-    endif
+    ! elseif (dirName(1:11) == './c180_data') then
+    !     IM = 180
+    !     JM = 180
+    !     LM = 72
+    ! endif
 
-    if (trim(dirName(12:23)) == 'radcoup_loop' .or. trim(dirName(13:24)) == 'radcoup_loop') then
+    ! if (trim(dirName(12:23)) == 'radcoup_loop' .or. trim(dirName(13:24)) == 'radcoup_loop') then
         call test_radcoup_loop(IM, JM, LM, dirName, rank_str)
-    endif
+    ! endif
 end program
 
 ! NASA Docket No. GSC-15,354-1, and identified as "GEOS-5 GCM Modeling Software”
