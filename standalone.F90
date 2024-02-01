645e819a4b5e2834c6701dbb9b15d313f44c6020
Christopher Kung <christopher.w.kung@nasa.gov>
2024-02-01 14:46:12 -0500

    Hard code dimensions of C180 standalone

--- standalone.F90
+++ standalone.F90
@@ -17,24 +17,24 @@ program test_moist_subroutines
         call get_command_argument(2, rank_str)
     endif
 
-    if (dirName(1:10) == './c24_data') then
-        IM = 24
-        JM = 24
-        LM = 72
-    elseif (dirName(1:10) == './c90_data') then
-        IM = 90
-        JM = 90
-        LM = 72
+    ! if (dirName(1:10) == './c24_data') then
+    !     IM = 24
+    !     JM = 24
+    !     LM = 72
+    ! elseif (dirName(1:10) == './c90_data') then
+    !     IM = 90
+    !     JM = 90
+    !     LM = 72
 
-    elseif (dirName(1:11) == './c180_data') then
+    ! elseif (dirName(1:11) == './c180_data') then
         IM = 180
         JM = 180
         LM = 72
-    endif
+    ! endif
 
-    if(trim(dirName(12:19)) == 'buoyancy' .or. trim(dirName(13:20)) == 'buoyancy') then
+    ! if(trim(dirName(12:19)) == 'buoyancy' .or. trim(dirName(13:20)) == 'buoyancy') then
         call test_BUOYANCY(IM, JM, LM, dirName, rank_str)
-    endif
+    ! endif
 
 end program
 
