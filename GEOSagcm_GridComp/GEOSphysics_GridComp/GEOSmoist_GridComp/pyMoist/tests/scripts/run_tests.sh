872c3cec5cc0c3add793be2473ba43edeea78acb
Matthew Weil <mattweil11@gmail.com>
2024-06-14 12:15:04 -0400

    Updating Testing data set

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PACE_FLOAT_PRECISION=32
-python -m pytest -v -s -x\
-    --data_path=../../test_data/geos_11.5.2/moist \
+python -m pytest --pdb -v -s -x\
+    --data_path=../../test_data/rad_coup_data_nc \
     --backend=numpy \
     --which_rank=0 \
     --which_modules=RadCouple \
