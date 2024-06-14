fa4962615265ea0dab22c5e5e8499236454d1874
Matthew Weil <mattweil11@gmail.com>
2024-06-14 12:34:25 -0400

    Got rid of --pdb

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PACE_FLOAT_PRECISION=32
-python -m pytest --pdb -v -s -x\
+python -m pytest -v -s -x\
     --data_path=../../test_data/rad_coup_data_nc \
     --backend=numpy \
     --which_rank=0 \
