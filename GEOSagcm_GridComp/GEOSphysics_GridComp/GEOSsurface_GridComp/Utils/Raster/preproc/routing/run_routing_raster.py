0d8b396b9c5571066b2da749430b8ff3216feff6
Rolf Reichle <rolf.reichle@nasa.gov>
2024-06-18 12:55:10 -0400

    cleaned up specification of path to bcs_shared directory (run_routing_raster.py)

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/routing/run_routing_raster.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/routing/run_routing_raster.py
@@ -5,22 +5,26 @@
 import os
 import subprocess
 
+# Path to "bcs_shared" directory:
+
+file_path  = "/discover/nobackup/projects/gmao/bcs_shared/"     # NCCS Discover
+#file_path = "/nobackup/gmao_SIteam/ModelData/bcs_shared/"      # NAS
+
 # Input files
-# For NAS users, please replace the Discover base path "/discover/nobackup/projects/gmao/bcs_shared/"
-# by the NAS path "/nobackup/gmao_SIteam/ModelData/bcs_shared/"
-file_Pfafcatch="/discover/nobackup/projects/gmao/bcs_shared/make_bcs_inputs/land/topo/v1/SRTM-TopoData/Pfafcatch-routing.dat"
-file_SRTMPfaf="/discover/nobackup/projects/gmao/bcs_shared/make_bcs_inputs/land/topo/v1/SRTM-TopoData/SRTM_PfafData.nc"
-file_Drainage="/discover/nobackup/projects/gmao/bcs_shared/preprocessing_bcs_inputs/land/routing/HydroSHEDS_drainage_area.nc"
-file_GrnLat="/discover/nobackup/projects/gmao/bcs_shared/preprocessing_bcs_inputs/land/routing/Greenland_outlets_lat.txt"
-file_GrnLon="/discover/nobackup/projects/gmao/bcs_shared/preprocessing_bcs_inputs/land/routing/Greenland_outlets_lon.txt"
-file_GrnMap="/discover/nobackup/projects/gmao/bcs_shared/preprocessing_bcs_inputs/land/routing/GreenlandID_30s.nc"
+
+file_Pfafcatch = file_path + "/make_bcs_inputs/land/topo/v1/SRTM-TopoData/Pfafcatch-routing.dat"
+file_SRTMPfaf  = file_path + "/make_bcs_inputs/land/topo/v1/SRTM-TopoData/SRTM_PfafData.nc"
+file_Drainage  = file_path + "/preprocessing_bcs_inputs/land/routing/HydroSHEDS_drainage_area.nc"
+file_GrnLat    = file_path + "/preprocessing_bcs_inputs/land/routing/Greenland_outlets_lat.txt"
+file_GrnLon    = file_path + "/preprocessing_bcs_inputs/land/routing/Greenland_outlets_lon.txt"
+file_GrnMap    = file_path + "/preprocessing_bcs_inputs/land/routing/GreenlandID_30s.nc"
 
 name_Pfafcatch = os.path.basename(file_Pfafcatch)
-name_SRTMPfaf = os.path.basename(file_SRTMPfaf)
-name_Drainage = os.path.basename(file_Drainage)
-name_GrnLat =os.path.basename(file_GrnLat)
-name_GrnLon = os.path.basename(file_GrnLon)
-name_GrnMap = os.path.basename(file_GrnMap)
+name_SRTMPfaf  = os.path.basename(file_SRTMPfaf)
+name_Drainage  = os.path.basename(file_Drainage)
+name_GrnLat    = os.path.basename(file_GrnLat)
+name_GrnLon    = os.path.basename(file_GrnLon)
+name_GrnMap    = os.path.basename(file_GrnMap)
 
 files=[file_Pfafcatch,file_SRTMPfaf,file_Drainage,file_GrnLat,file_GrnLon,file_GrnMap]
 # Remove files and directories
