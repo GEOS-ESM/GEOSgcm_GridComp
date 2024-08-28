7b8363ddd08d74d523ceba75385202a9ccdc44ae
Katrina Fandrich <kf041796@kfandrichlap.univ.albany.edu>
2024-08-28 13:53:02 -0400

    [NDSL] Porting slope and conden in UW_Convection Port

---
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/conden.py
@@ -0,0 +1,182 @@
+import gt4py.cartesian.gtscript as gtscript
+from gt4py.cartesian.gtscript import computation, interval, PARALLEL, sin
+import pyMoist.radiation_coupling_constants as radconstants
+import pyMoist.pyMoist_constants as constants
+from ndsl.constants import X_DIM, Y_DIM, Z_DIM
+from ndsl.dsl.typing import FloatField, Float, IntField 
+from ndsl import StencilFactory, QuantityFactory 
+
+
+@gtscript.function
+def exnerfn(
+    p: Float,
+)-> Float:
+    
+    return (p / 100000.0) ** (radconstants.MAPL_RDRY / radconstants.MAPL_CPDRY)
+
+
+@gtscript.function
+def geos_qsat(
+    temps: Float,
+    ps: Float, 
+)-> Float:
+    
+    # This is just a placeholder until GEOS_QSAT is ported!!!
+    e_s0 = 6.11         # Reference vapor pressure (hPa)
+    a = 17.67           # Constant for temperature in the exponential function
+    b = 273.15          # Reference temperature (K)
+    c = 29.65           # Temperature constant in the denominator (K)
+    q_s0 = 0.62         # Ratio of molecular weights of water vapor and dry air (dimensionless)
+    
+    # Saturation vapor pressure calculation
+    e_s = e_s0 * temps
+    
+    # Saturation specific humidity calculation
+    q_s = q_s0 * e_s / (ps - e_s)
+    
+    return q_s
+
+
+@gtscript.function
+def ice_fraction(
+    temp: Float,
+    cnv_frc: Float,
+    srf_type: Float,
+):
+    # Anvil clouds
+    # Anvil-Convective sigmoidal function like figure 6(right)
+    # Sigmoidal functions Hu et al 2010, doi:10.1029/2009JD012384
+    if temp <= constants.JaT_ICE_ALL:
+        icefrct_c = 1.000
+    elif temp > constants.JaT_ICE_ALL and temp <= constants.JaT_ICE_MAX:
+        icefrct_c = sin(0.5 * radconstants.MAPL_PI * (1.00 - (temp - constants.JaT_ICE_ALL) / (constants.JaT_ICE_MAX - constants.JaT_ICE_ALL)))
+    else:
+        icefrct_c = 0.00
+    icefrct_c = max(min(icefrct_c,1.00),0.00) ** constants.aICEFRPWR
+    # Sigmoidal functions like figure 6b/6c of Hu et al 2010, doi:10.1029/2009JD012384
+    if srf_type == 2.0:
+        if temp <= constants.JiT_ICE_ALL:
+            icefrct_m = 1.000
+        elif temp > constants.JiT_ICE_ALL and temp <= constants.JiT_ICE_MAX:
+            icefrct_m = 1.00 - (temp - constants.JiT_ICE_ALL) / (constants.JiT_ICE_MAX - constants.JiT_ICE_ALL)
+        else:
+            icefrct_m = 0.00
+        icefrct_m = max(min(icefrct_m, 1.00), 0.00) ** constants.iICEFRPWR
+    elif srf_type > 1.0:
+        if temp <= constants.lT_ICE_ALL:
+           icefrct_m = 1.000
+        elif temp > constants.lT_ICE_ALL and temp <= constants.lT_ICE_MAX:
+           icefrct_m = sin(0.5 * radconstants.MAPL_PI * (1.00 - (temp - constants.lT_ICE_ALL) / (constants.lT_ICE_MAX - constants.lT_ICE_ALL)))
+        else:
+            icefrct_m = 0.00
+        icefrct_m = max(min(icefrct_m, 1.00), 0.00) ** constants.lICEFRPWR
+    else:
+        if temp <= constants.oT_ICE_ALL:
+           icefrct_m = 1.000
+        elif temp > constants.oT_ICE_ALL and temp <= constants.oT_ICE_MAX:
+           icefrct_m = sin(0.5 * radconstants.MAPL_PI * (1.00 - (temp - constants.oT_ICE_ALL) / (constants.oT_ICE_MAX - constants.oT_ICE_ALL)))
+        else:
+            icefrct_m = 0.00
+        icefrct_m = max(min(icefrct_m, 1.00), 0.00) ** constants.oICEFRPWR
+    ice_frac = icefrct_m * (1.0-cnv_frc) + icefrct_c * cnv_frc
+    return ice_frac
+
+
+def condensation(
+    p: FloatField, 
+    thl: FloatField, 
+    qt: FloatField,
+    th: FloatField, 
+    qv: FloatField, 
+    ql: FloatField,
+    qi: FloatField, 
+    rvls: FloatField,
+    id_check: IntField,
+):
+
+    with computation(PARALLEL), interval(...):
+
+        tc = thl * exnerfn(p)
+        
+        icefrac = ice_fraction(tc, 0.0, 0.0)
+        nu = icefrac
+        leff = (1.0 - nu) * radconstants.MAPL_ALHL + nu * radconstants.MAPL_ALHS # Effective latent heat
+        temps = tc
+        ps = p
+        qs = geos_qsat(temps, ps / 100.0) # Saturation specific humidity
+        rvls = qs
+        
+        if qs >= qt:      # no condensation
+            id_check = 0
+            qv = qt
+            qc = 0.0
+            ql = 0.0
+            qi = 0.0
+            th = thl
+        else:             # condensation
+            iteration = 0
+            while iteration < 10:
+                temps = temps + ((tc - temps) * radconstants.MAPL_CPDRY / leff + qt - rvls) / (radconstants.MAPL_CPDRY / leff + (constants.rdry / constants.rvap) * leff * rvls / (radconstants.MAPL_RDRY * temps * temps))
+                qs = geos_qsat(temps, ps / 100.0)
+                rvls = qs
+                iteration+=1
+            qc = max(qt - qs, 0.0)
+            qv = qt - qc
+            ql = qc * (1.0 - nu)
+            qi = nu * qc
+            th = temps / exnerfn(p)
+            if abs((temps - (leff / radconstants.MAPL_CPDRY) * qc) - tc) >= 1.0:
+                id_check = 1
+            else:
+                id_check = 0
+                
+
+
+class Conden:
+    def __init__(
+        self,
+        stencil_factory: StencilFactory,
+        quantity_factory: QuantityFactory,
+    ):
+      
+        self._condensation = stencil_factory.from_dims_halo(
+            func=condensation,
+            compute_dims=[X_DIM, Y_DIM, Z_DIM],
+        )
+
+    def __call__(self,
+        pifc0_test: FloatField, 
+        thl0bot_test: FloatField, 
+        qt0bot_test: FloatField,
+        thj_test: FloatField, 
+        qvj_test: FloatField, 
+        qlj_test: FloatField,
+        qij_test: FloatField, 
+        qse_test: FloatField,
+        id_check_test: IntField,
+    ):
+
+        """
+            Calculate condensation process.
+
+            Parameters:
+            p (3D in): Environmental pressure [ Pa ].
+            thl (3D in): 
+            qt (3D in):
+            th (3D out):
+            qv (3D out):
+            ql (3D out):
+            qi (3D out):
+            rvls (3D out):
+            id_check (3D out): Indicates if condensation process occurs [ N/A ].
+        """
+        self._condensation(
+                pifc0_test, 
+                thl0bot_test, 
+                qt0bot_test, 
+                thj_test, 
+                qvj_test, 
+                qlj_test, 
+                qij_test, 
+                qse_test, 
+                id_check_test)
---
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/savepoint/translate_slope.py
@@ -0,0 +1,78 @@
+from ndsl import Namelist, StencilFactory, QuantityFactory 
+from ndsl.constants import X_DIM, Y_DIM, Z_DIM
+from ndsl.stencils.testing.translate import TranslateFortranData2Py
+from pyMoist.slope import Slope
+import numpy as np
+
+
+class TranslateSlope(TranslateFortranData2Py):
+    def __init__(
+        self,
+        grid,
+        namelist: Namelist,
+        stencil_factory: StencilFactory,
+    ):
+        super().__init__(grid, stencil_factory)
+        self.compute_func = Slope(
+            self.stencil_factory,
+            self.grid.quantity_factory,
+        )
+
+        self.max_error = 1e-9
+
+        print(grid.__dict__)
+
+        # FloatField Inputs
+        self.in_vars["data_vars"] = {
+            "thl0_test": {},
+            "pmid0_test": {},
+        }
+
+        print(self.in_vars["data_vars"])
+
+        # FloatField Outputs
+        self.out_vars = {
+            "ssthl0_test": self.grid.compute_dict(),
+        }
+
+    def reshape_before(self,inputs):
+        # Reshape input fields from (i*j, k) to (i, j, k)
+        i, j, k = self.grid.nic, self.grid.njc, self.grid.npz 
+        reshaped_inputs = {key: np.reshape(inputs[key], (i, j, k)).astype(np.float32) for key in inputs}
+    
+        # Add halo of 3 in i and j dimensions
+        halo_arrays = {}
+        for key, array in reshaped_inputs.items():
+            new_array = np.zeros((i+6, j+6, k), dtype=np.float32)
+            new_array[3:-3, 3:-3, :] = array
+
+            halo_arrays[key] = new_array
+
+        return halo_arrays
+
+    def reshape_after(self,outputs):
+        # Reshape output fields from (i, j, k) back to (i*j, k)
+        i, j, k = self.grid.nic, self.grid.njc, self.grid.npz
+        reshaped_outputs = np.reshape(outputs, (i*j, k))
+
+        return reshaped_outputs
+        
+
+    # Calculated Outputs
+    def compute(self, inputs):
+
+        inputs_2D = self.reshape_before(inputs)
+        print("Reshaped 2D fields to 3D")
+
+        outputs = {
+            "ssthl0_test": self.grid.quantity_factory.zeros(dims=[X_DIM, Y_DIM, Z_DIM], units="unknown"),
+        }
+     
+        self.compute_func(**inputs_2D,**outputs)
+        print("Calculated slope on 3D fields")
+
+        outputs_2D = self.reshape_after(outputs["ssthl0_test"].view[:,:,:])
+        print("Reshaped back to 2D")
+
+        return {"ssthl0_test": outputs_2D
+            }
