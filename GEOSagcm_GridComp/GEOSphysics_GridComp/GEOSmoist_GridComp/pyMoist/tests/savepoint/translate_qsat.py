c138b46d37e1162be266a85e634bf081a2201a12
Florian Deconinck <florian.deconinck@gmail.com>
2024-09-13 12:55:41 -0400

    Clean up translate test Revert run_tests.sh to common one

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/savepoint/translate_qsat.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/savepoint/translate_qsat.py
@@ -1,9 +1,7 @@
-import xarray as xr
-
 from ndsl import Namelist, Quantity, StencilFactory
 from ndsl.constants import X_DIM, Y_DIM, Z_DIM
 from ndsl.stencils.testing.translate import TranslateFortranData2Py
-from pyMoist.saturation.qsat import QSat
+from pyMoist.saturation import QSat, TableMethod
 
 
 class TranslateQSat(TranslateFortranData2Py):
@@ -22,13 +20,6 @@ class TranslateQSat(TranslateFortranData2Py):
         self.in_vars["data_vars"] = {
             "PL": {},
             "T": {},
-            "ESTBLE_TEST": {},
-            "ESTBLW_TEST": {},
-            "ESTBLX_TEST": {},
-            "IT": {},
-            "IFELSE": {},
-            "QSAT_HALFWAY": {},
-            "TI": {},
         }
 
         # Float Inputs
@@ -37,14 +28,6 @@ class TranslateQSat(TranslateFortranData2Py):
         # FloatField Outputs
         self.out_vars = {
             "QSAT": self.grid.compute_dict(),
-            "ESTBLE_TEST": self.grid.compute_dict(),
-            "ESTBLW_TEST": self.grid.compute_dict(),
-            "ESTBLX_TEST": self.grid.compute_dict(),
-            # "IT": self.grid.compute_dict(),
-            # "IFELSE": self.grid.compute_dict(),
-            # "QSAT_HALFWAY": self.grid.compute_dict(),
-            # "TI": self.grid.compute_dict(),
-            # "LOC": self.grid.compute_dict(),
         }
 
     def make_ij_field(self, data) -> Quantity:
@@ -72,41 +55,21 @@ class TranslateQSat(TranslateFortranData2Py):
         return qty
 
     def compute(self, inputs):
-        code = QSat(
+        qsat = QSat(
             self.stencil_factory,
             self.quantity_factory,
+            table_method=TableMethod.NetCDF,
         )
 
         # FloatField Variables
         T = self.make_ijk_field(inputs["T"])
         PL = self.make_ijk_field(inputs["PL"])
 
-        with xr.open_dataset("/home/charleskrop/netcdfs/QSat-Out.nc") as ds:
-            ese_array = ds.data_vars["ESTBLE_TEST"].values[0, 0, :]
-            esw_array = ds.data_vars["ESTBLW_TEST"].values[0, 0, :]
-            esx_array = ds.data_vars["ESTBLX_TEST"].values[0, 0, :]
-
-        # FloatField_Extra_Dim Variables
-        ese = self.make_extra_dim_field(ese_array)
-        esw = self.make_extra_dim_field(esw_array)
-        esx = self.make_extra_dim_field(esx_array)
-
-        code(
+        qsat(
             T,
             PL,
-            # ese=ese,
-            # esw=esw,
-            # esx=esx,
         )
 
         return {
-            "QSAT": code.QSat.view[:],
-            "ESTBLE_TEST": code.ese.view[0],
-            "ESTBLW_TEST": code.esw.view[0],
-            "ESTBLX_TEST": code.esx.view[0],
-            # "IT": code._IT.view[:],
-            # "IFELSE": code._IFELSE.view[:],
-            # "QSAT_HALFWAY": code._QSAT_HALFWAY.view[:],
-            # "TI": code._TI.view[:],
-            # "LOC": code.table._LOC,
+            "QSAT": qsat.QSat.view[:],
         }
