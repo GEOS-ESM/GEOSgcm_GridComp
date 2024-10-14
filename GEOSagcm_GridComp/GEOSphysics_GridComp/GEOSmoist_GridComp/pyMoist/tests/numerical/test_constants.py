12ecd4e4573aedc67a6f54bf48c510bf79823bd0
Florian Deconinck <deconinck.florian@gmail.com>
2024-10-14 09:28:01 -0400

    Rename cast ops to be private Trim const list for test

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/numerical/test_constants.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/numerical/test_constants.py
@@ -9,18 +9,24 @@ def _check_dtype(np_var, py_or_np_var):
     return np_var.dtype == py_or_np_var.dtype
 
 
+def _get_constant_from_module():
+    # All public module var
+    const_module_var = [item for item in dir(const) if not item.startswith("_")]
+    # Get rid of the imports
+    const_module_var.remove("np")
+    const_module_var.remove("Float")
+    return const_module_var
+
+
 if __name__ == "__main__":
     import xarray as xr
 
     ds = xr.open_dataset(
         "/home/fgdeconi/work/git/fp/geos/src/Components/@GEOSgcm_GridComp/GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/test_data/11.5.2/Moist/TBC_C24_L72_Debug/Constants-Out.nc"
     )
-
-    all_public_const_module_var = [
-        item for item in dir(const) if not item.startswith("__")
-    ]
+    const_module_var = _get_constant_from_module()
     unchecked_vars = set()
-    for v in all_public_const_module_var:
+    for v in const_module_var:
         if v in ds.keys():
             f90 = ds[v][0, 0].to_numpy()
             py = getattr(const, v)
