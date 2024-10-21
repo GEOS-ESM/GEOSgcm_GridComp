c1f85b0afff1052d32fff367b2e8e68d00e1e271
Florian Deconinck <deconinck.florian@gmail.com>
2024-10-21 11:11:54 -0400

    Update `get_data` to new path

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/get_data.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/get_data.sh
@@ -5,4 +5,4 @@ set -e -x
 TEST_DATA_PATH="../../test_data/" #11.5.2/Moist/
 mkdir -p $TEST_DATA_PATH
 cd $TEST_DATA_PATH
-wget -r -nH --cut-dir=5 -np -R "index.html*" https://portal.nccs.nasa.gov/datashare/astg/smt/geos-fp/translate/11.5.2/Moist/TBC_C24_L72_Debug/
+wget -r -nH --cut-dir=5 -np -R "index.html*" https://portal.nccs.nasa.gov/datashare/astg/smt/geos-fp/translate/11.5.2/x86_GNU/Moist/TBC_C24_L72_Debug/
