651b3cdccc23e189849f495710e12b6b1a32679c
gmao-jkolassa <jana.kolassa@nasa.gov>
2024-11-19 13:33:32 -0500

    add root profile initialization

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/RootBiophysMod.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/CLM51/RootBiophysMod.F90
@@ -12,7 +12,7 @@ module RootBiophysMod
   private
   !
   public  :: init_vegrootfr
-!  public  :: init_rootprof
+  public  :: init_rootprof
 
   integer, private, parameter :: zeng_2001_root    = 0 !the zeng 2001 root profile function
   integer, private, parameter :: jackson_1996_root = 1 !the jackson 1996 root profile function
@@ -31,80 +31,80 @@ module RootBiophysMod
 contains
 
   !-------------------------------------------------------------------------------------- 
-!  subroutine init_rootprof(NLFilename)
-!    !
-!    !DESCRIPTION
-!    ! initialize methods for root profile calculation
-!
-!    ! !USES:
-!    use abortutils      , only : endrun   
-!    use fileutils       , only : getavu, relavu
-!    use spmdMod         , only : mpicom, masterproc
-!    use shr_mpi_mod     , only : shr_mpi_bcast
-!    use clm_varctl      , only : iulog
-!    use clm_nlUtilsMod  , only : find_nlgroup_name
-!
-!    ! !ARGUMENTS:
-!    !------------------------------------------------------------------------------
-!    implicit none
-!    character(len=*), intent(in) :: NLFilename
-!
-!    integer            :: nu_nml                     ! unit for namelist file
-!    integer            :: nml_error                  ! namelist i/o error flag
-!    character(*), parameter    :: subName = "('init_rootprof')"
-!
-!    !-----------------------------------------------------------------------
-!
-!! MUST agree with name in namelist and read statement
-!    namelist /rooting_profile_inparm/ rooting_profile_method_water, rooting_profile_method_carbon, &
-!                                      rooting_profile_varindex_water, rooting_profile_varindex_carbon
-!
-!    ! Default values for namelist
-!
-!    rooting_profile_method_water    = zeng_2001_root
-!    rooting_profile_method_carbon   = zeng_2001_root
-!    rooting_profile_varindex_water  = 1
-!    rooting_profile_varindex_carbon = 2
-!
-!    ! Read rooting_profile namelist
-!    if (masterproc) then
-!       nu_nml = getavu()
-!       open( nu_nml, file=trim(NLFilename), status='old', iostat=nml_error )
-!       call find_nlgroup_name(nu_nml, 'rooting_profile_inparm', status=nml_error)
-!       if (nml_error == 0) then
-!          read(nu_nml, nml=rooting_profile_inparm,iostat=nml_error)
-!          if (nml_error /= 0) then
-!             call endrun(subname // ':: ERROR reading rooting_profile namelist')
-!          end if
-!       else
-!          call endrun(subname // ':: ERROR finding rooting_profile namelist')
-!       end if
-!       close(nu_nml)
-!       call relavu( nu_nml )
-!
-!    endif
-!
-!    call shr_mpi_bcast(rooting_profile_method_water,    mpicom)
-!    call shr_mpi_bcast(rooting_profile_method_carbon,   mpicom)
-!    call shr_mpi_bcast(rooting_profile_varindex_water,  mpicom)
-!    call shr_mpi_bcast(rooting_profile_varindex_carbon, mpicom)
-!
-!    if (masterproc) then
-!
-!       write(iulog,*) ' '
-!       write(iulog,*) 'rooting_profile settings:'
-!       write(iulog,*) '  rooting_profile_method_water  = ',rooting_profile_method_water
-!       if ( rooting_profile_method_water == jackson_1996_root )then
-!          write(iulog,*) '  (rooting_profile_varindex_water  = ',rooting_profile_varindex_water, ')'
-!       end if
-!       write(iulog,*) '  rooting_profile_method_carbon = ',rooting_profile_method_carbon
-!       if ( rooting_profile_method_carbon == jackson_1996_root )then
-!          write(iulog,*) '  (rooting_profile_varindex_carbon  = ',rooting_profile_varindex_carbon, ')'
-!       end if
-!
-!    endif
-!
-!  end subroutine init_rootprof
+  subroutine init_rootprof(NLFilename)
+    !
+    !DESCRIPTION
+    ! initialize methods for root profile calculation
+
+    ! !USES:
+    use abortutils      , only : endrun   
+    use fileutils       , only : getavu, relavu
+    use spmdMod         , only : mpicom, masterproc
+    use shr_mpi_mod     , only : shr_mpi_bcast
+    use clm_varctl      , only : iulog
+    use clm_nlUtilsMod  , only : find_nlgroup_name
+
+    ! !ARGUMENTS:
+    !------------------------------------------------------------------------------
+    implicit none
+    character(len=*), intent(in) :: NLFilename
+
+    integer            :: nu_nml                     ! unit for namelist file
+    integer            :: nml_error                  ! namelist i/o error flag
+    character(*), parameter    :: subName = "('init_rootprof')"
+
+    !-----------------------------------------------------------------------
+
+! MUST agree with name in namelist and read statement
+    namelist /rooting_profile_inparm/ rooting_profile_method_water, rooting_profile_method_carbon, &
+                                      rooting_profile_varindex_water, rooting_profile_varindex_carbon
+
+    ! Default values for namelist
+
+    rooting_profile_method_water    = zeng_2001_root
+    rooting_profile_method_carbon   = zeng_2001_root
+    rooting_profile_varindex_water  = 1
+    rooting_profile_varindex_carbon = 2
+
+    ! Read rooting_profile namelist
+    if (masterproc) then
+       nu_nml = getavu()
+       open( nu_nml, file=trim(NLFilename), status='old', iostat=nml_error )
+       call find_nlgroup_name(nu_nml, 'rooting_profile_inparm', status=nml_error)
+       if (nml_error == 0) then
+          read(nu_nml, nml=rooting_profile_inparm,iostat=nml_error)
+          if (nml_error /= 0) then
+             call endrun(subname // ':: ERROR reading rooting_profile namelist')
+          end if
+       else
+          call endrun(subname // ':: ERROR finding rooting_profile namelist')
+       end if
+       close(nu_nml)
+       call relavu( nu_nml )
+
+    endif
+
+    call shr_mpi_bcast(rooting_profile_method_water,    mpicom)
+    call shr_mpi_bcast(rooting_profile_method_carbon,   mpicom)
+    call shr_mpi_bcast(rooting_profile_varindex_water,  mpicom)
+    call shr_mpi_bcast(rooting_profile_varindex_carbon, mpicom)
+
+    if (masterproc) then
+
+       write(iulog,*) ' '
+       write(iulog,*) 'rooting_profile settings:'
+       write(iulog,*) '  rooting_profile_method_water  = ',rooting_profile_method_water
+       if ( rooting_profile_method_water == jackson_1996_root )then
+          write(iulog,*) '  (rooting_profile_varindex_water  = ',rooting_profile_varindex_water, ')'
+       end if
+       write(iulog,*) '  rooting_profile_method_carbon = ',rooting_profile_method_carbon
+       if ( rooting_profile_method_carbon == jackson_1996_root )then
+          write(iulog,*) '  (rooting_profile_varindex_carbon  = ',rooting_profile_varindex_carbon, ')'
+       end if
+
+    endif
+
+  end subroutine init_rootprof
 
   !-------------------------------------------------------------------------------------- 
   subroutine init_vegrootfr(bounds, nlevsoi, nlevgrnd, rootfr, water_carbon)
