43ac84b7efaf74aec0e16aeaacbc4dcddba8b203
Charles Kropiewnicki <charles.j.krop@gmail.com>
2025-02-18 16:46:20 -0500

    Revert run_tests.sh

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_tests.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
-clear
-# rm -rf ./.gt_cache_*
-rm -rf ./.translate-*
-export GT4PY_LITERAL_PRECISION=32
-export FV3_DACEMODE=Python
+rm -rf ./.gt_cache_*
 export PACE_FLOAT_PRECISION=32
-export PACE_TEST_N_THRESHOLD_SAMPLES=0
-export PACE_DACE_DEBUG=True
-export GT4PY_COMPILE_OPT_LEVEL=0
-# for debugging only
-export OMP_NUM_THREADS=1
-python -m pytest -s \
-    --data_path=/Users/ckropiew/netcdfs \
-    --backend=gt:cpu_kfirst \
+export FV3_DACEMODE=Python
+python -m pytest -v -s -x\
+    --data_path=../../test_data/11.5.2/Moist/TBC_C24_L72_Debug \
+    --backend=dace:cpu \
     --grid=default \
     --multimodal_metric \
-    --which_modules=terminal_fall \
-    --which_rank=0 \
     ..
