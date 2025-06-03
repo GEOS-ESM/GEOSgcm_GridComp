76cff353d221a869b2efa47b5c40c5c526d7c8de
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2025-06-03 10:14:21 -0400

    refactoring...

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/makebcs/mkCatchParam.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/makebcs/mkCatchParam.F90
@@ -1,3 +1,6 @@
+#define I_AM_MAIN
+#include "MAPL_ErrLog.h"
+
 PROGRAM mkCatchParam
 
 ! !INTERFACE:
@@ -21,6 +24,7 @@ PROGRAM mkCatchParam
   use MAPL
   use rmTinyCatchParaMod 
   use process_hres_data
+  use MAPL_ExceptionHandling
 
   !   use module_irrig_params, ONLY : create_irrig_params
 
@@ -189,7 +193,7 @@ integer :: n_threads=1
   
    if (index(Gridname,'EASEv') /=0) then
       ! here Gridname has alias EASELabel
-      call ease_extent(Gridname, nc_ease, nr_ease )
+      call ease_extent(Gridname, nc_ease, nr_ease, _RC)
       write(nc_string, '(i0)') nc_ease
       write(nr_string, '(i0)') nr_ease
       Gridname = trim(Gridname)//'_'//trim(nc_string)//'x'//trim(nr_string)
