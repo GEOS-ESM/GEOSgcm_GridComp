68cffdaefdeb2cb777b9492a8e1622e4bedf8baa
gmao-jkolassa <jana.kolassa@nasa.gov>
2025-07-01 10:12:41 -0400

    change default settings for accumulation flags

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/GEOS_CatchCNCLM51GridComp.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/GEOSland_GridComp/GEOScatchCN_GridComp/GEOScatchCNCLM51_GridComp/GEOS_CatchCNCLM51GridComp.F90
@@ -5246,8 +5246,8 @@ subroutine RUN2 ( GC, IMPORT, EXPORT, CLOCK, RC )
     ! NOTE: In CNPhenologyMod.F90, init_gdd20 is always set to .false. as well. For GEOS-5 runs, need to discard at least the first 2 years.
     ! This is not a problem for offline runs because we always spin up the model whenever we change meterology. fzeng, July 2017 
     ! -------------------------------------------------------------------------------------------------------------------------------------- 
-    logical, parameter :: init_accum = .true.! jkolassa May 2023: needs to be set to true if no CNCLM51 restart is available
-    logical, parameter :: init_accum_365 = .true.! jkolassa May 2023: needs to be set to true if no CNCLM51 restart is available
+    logical, parameter :: init_accum = .false.! jkolassa May 2023: needs to be set to true if no CNCLM51 restart is available
+    logical, parameter :: init_accum_365 = .false.! jkolassa May 2023: needs to be set to true if no CNCLM51 restart is available
     integer, save :: istep                    ! model time step index
     integer, save :: istep_365                ! model time step index
     integer :: accper                         ! number of time steps accumulated in a period of XX days, increases from 1 to nXXd in the first XX days,
