8a4378159b98fb56195f29310fcf72d81d968899
Charles Kropiewnicki <charles.j.krop@gmail.com>
2025-10-28 11:22:19 -0400

    Fixing some things after merge

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/cumulus_parameterization/constants.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/cumulus_parameterization/constants.py
@@ -2,21 +2,21 @@ from ndsl.dsl.typing import Float, Int
 
 
 # physical constants used ONLY for GF_2020, else there may be unintended conflicts with global constants
-RGAS = 287.0  # J K-1 kg-1
-CP = 1004.0  # J K-1 kg-1
-RV = 461.0  # J K-1 kg-1
-P00 = 1.0e5  # hPa
-TCRIT = 258.0  # K
+RGAS = Float(287.0)  # J K-1 kg-1
+CP = Float(1004.0)  # J K-1 kg-1
+RV = Float(461.0)  # J K-1 kg-1
+P00 = Float(1.0e5)  # hPa
+TCRIT = Float(258.0)  # K
 CPOR = CP / RGAS
-XLV = 2.5e6  # J kg-1
-AKMIN = 1.0  # #
-TKMIN = 1.0e-5  # m+2 s-2
-CCNCLEAN = 250.0  # # cm-3
-T_0 = 273.16  # K
-T_ICE = 235.16  # K
-XLF = 0.333e6  # latent heat of freezing (J K-1 kg-1)
-MAX_QSAT = 0.5  # kg/kg
-MX_BUOY = CP * 5.0 + XLV * 2.0e-3  # temp exc=5 K, q deficit=2 g/kg (=> mx_buoy ~ 10 kJ/kg)
+XLV = Float(2.5e6)  # J kg-1
+AKMIN = Float(1.0)  # #
+TKMIN = Float(1.0e-5)  # m+2 s-2
+CCNCLEAN = Float(250.0)  # # cm-3
+T_0 = Float(273.16)  # K
+T_ICE = Float(235.16)  # K
+XLF = Float(0.333e6)  # latent heat of freezing (J K-1 kg-1)
+MAX_QSAT = Float(0.5)  # kg/kg
+MX_BUOY = CP * Float(5.0) + XLV * Float(2.0e-3)  # temp exc=5 K, q deficit=2 g/kg (=> mx_buoy ~ 10 kJ/kg)
 
 smaller_qv = Float(1.0e-16)  # kg/kg
 
