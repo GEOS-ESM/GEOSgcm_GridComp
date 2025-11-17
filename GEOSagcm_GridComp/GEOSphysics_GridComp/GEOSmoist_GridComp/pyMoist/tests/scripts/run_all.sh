6d528712e870883587ef10590aa67074021d7b4f
Florian Deconinck <deconinck.florian@gmail.com>
2025-11-17 13:55:17 -0500

    Update to run scripts for tests + new "run all" Clean up of __init__ of tests

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_all.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/tests/scripts/run_all.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Usage: ./run_tests.sh [/path/to/data] [debug|dace:cpu_kfirst] [TranslateName]
+# Usage: ./run_tests.sh [/path/to/data] [debug|dace:cpu_kfirst]
 
 export NDSL_LITERAL_PRECISION=32
 export NDSL_TEST_N_THRESHOLD_SAMPLES=0
 export GT4PY_COMPILE_OPT_LEVEL=0
 export FV3_DACEMODE=Python
+export NDSL_LOGLEVEL=Critical
 
 # UW specific
 export GT4PY_EXTRA_COMPILE_OPT_FLAGS='-fconstexpr-ops-limit=1000000000'
 
-python -m pytest -s -v --disable-warnings --multimodal_metric \
-    -x \
+python -m pytest -s --disable-warnings --multimodal_metric \
+    -x -v \
     --data_path=$1 \
     --backend=$2\
-    --which_modules=$3 \
     --grid=default \
     --no_report \
     --threshold_overrides_file=./overrides.yml \
     ..
-
+    
