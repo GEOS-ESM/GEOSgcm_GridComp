89d481208b1d39487aa4175ce349f067961f0cca
Florian Deconinck <deconinck.florian@gmail.com>
2025-12-11 13:28:49 -0500

    Move GFDL1M to locals

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/GFDL_1M/GFDL_1M.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/GFDL_1M/GFDL_1M.py
@@ -1,4 +1,4 @@
-from ndsl import QuantityFactory, StencilFactory
+from ndsl import NDSLRuntime, QuantityFactory, StencilFactory
 from ndsl.constants import X_DIM, Y_DIM, Z_DIM
 from pyMoist.GFDL_1M.config import GFDL1MConfig
 from pyMoist.GFDL_1M.driver.driver import GFDL1MDriver
@@ -16,7 +16,7 @@ from pyMoist.GFDL_1M.stencils import (
 from pyMoist.saturation_tables import get_saturation_vapor_pressure_table
 
 
-class GFDL1M:
+class GFDL1M(NDSLRuntime):
     """
     GFDL Single Moment microphysics
 
@@ -48,6 +48,9 @@ class GFDL1M:
 
         # Initalize saturation tables
         saturation_tables = get_saturation_vapor_pressure_table(stencil_factory.backend)
+
+        # Locals
+        self._locals = GFDL1MLocals.make_locals(quantity_factory)
 
         # Build components
         self.prepare_tendencies = stencil_factory.from_dims_halo(
@@ -108,7 +111,6 @@ class GFDL1M:
     def __call__(
         self,
         state: GFDL1MState,
-        locals: GFDL1MLocals,
     ):
         # NOTE test GFDL_1M_setup: ❌ fails with tiny error (operationalå differences?)
         self._setup(
@@ -142,20 +144,20 @@ class GFDL1M:
             draindt_macro=state.tendencies.draindt_macro,
             dsnowdt_macro=state.tendencies.dsnowdt_macro,
             dgraupeldt_macro=state.tendencies.dgraupeldt_macro,
-            local_p_mb=locals.p_mb,
-            local_p_interface_mb=locals.p_interface_mb,
-            local_edge_height_above_surface=locals.edge_height_above_surface,
-            local_layer_height_above_surface=locals.layer_height_above_surface,
-            local_layer_thickness=locals.layer_thickness,
-            local_layer_thickness_negative=locals.layer_thickness_negative,
-            local_dp=locals.dp,
-            local_mass=locals.mass,
-            local_mass_inverse=locals.mass_inverse,
-            local_saturation_specific_humidity=locals.saturation_specific_humidity,
-            local_dsaturation_specific_humidity=locals.dsaturation_specific_humidity,
-            local_u_unmodified=locals.u_unmodified,
-            local_v_unmodified=locals.v_unmodified,
-            local_lcl_level=locals.lcl_level,
+            local_p_mb=self._locals.p_mb,
+            local_p_interface_mb=self._locals.p_interface_mb,
+            local_edge_height_above_surface=self._locals.edge_height_above_surface,
+            local_layer_height_above_surface=self._locals.layer_height_above_surface,
+            local_layer_thickness=self._locals.layer_thickness,
+            local_layer_thickness_negative=self._locals.layer_thickness_negative,
+            local_dp=self._locals.dp,
+            local_mass=self._locals.mass,
+            local_mass_inverse=self._locals.mass_inverse,
+            local_saturation_specific_humidity=self._locals.saturation_specific_humidity,
+            local_dsaturation_specific_humidity=self._locals.dsaturation_specific_humidity,
+            local_u_unmodified=self._locals.u_unmodified,
+            local_v_unmodified=self._locals.v_unmodified,
+            local_lcl_level=self._locals.lcl_level,
         )
 
         self._phase_change(
@@ -178,10 +180,10 @@ class GFDL1M:
             cloud_ice_sublimation=state.cloud_ice_sublimation,
             convection_fraction=state.convection_fraction,
             surface_type=state.surface_type,
-            local_lcl_level=locals.lcl_level,
-            local_p_mb=locals.p_mb,
-            local_p_interface_mb=locals.p_interface_mb,
-            local_saturation_specific_humidity=locals.saturation_specific_humidity,
+            local_lcl_level=self._locals.lcl_level,
+            local_p_mb=self._locals.p_mb,
+            local_p_interface_mb=self._locals.p_interface_mb,
+            local_saturation_specific_humidity=self._locals.saturation_specific_humidity,
         )
 
         self._update_tendencies(
@@ -262,8 +264,8 @@ class GFDL1M:
             u=state.u,
             v=state.v,
             w=state.vertical_motion.velocity,
-            dz=locals.dz,
-            dp=locals.dp,
+            dz=self._locals.dz,
+            dp=self._locals.dp,
             area=state.area,
             land_fraction=state.land_fraction,
             convection_fraction=state.convection_fraction,
@@ -277,17 +279,17 @@ class GFDL1M:
             snow=state.radiation_field.snow,
             graupel=state.radiation_field.graupel,
             cloud_fraction=state.radiation_field.cloud_fraction,
-            total_concentration=locals.total_concentration,
-            dvapordt=locals.driver_tencencies.dvapordt,
-            dliquiddt=locals.driver_tencencies.dliquiddt,
-            draindt=locals.driver_tencencies.draindt,
-            dicedt=locals.driver_tencencies.dicedt,
-            dsnowdt=locals.driver_tencencies.dsnowdt,
-            dgraupeldt=locals.driver_tencencies.dgraupeldt,
-            dcloudfractiondt=locals.driver_tencencies.dcloudfractiondt,
-            dtdt=locals.driver_tencencies.dtdt,
-            dudt=locals.driver_tencencies.dudt,
-            dvdt=locals.driver_tencencies.dvdt,
+            total_concentration=self._locals.total_concentration,
+            dvapordt=self._locals.driver_tencencies.dvapordt,
+            dliquiddt=self._locals.driver_tencencies.dliquiddt,
+            draindt=self._locals.driver_tencencies.draindt,
+            dicedt=self._locals.driver_tencencies.dicedt,
+            dsnowdt=self._locals.driver_tencencies.dsnowdt,
+            dgraupeldt=self._locals.driver_tencencies.dgraupeldt,
+            dcloudfractiondt=self._locals.driver_tencencies.dcloudfractiondt,
+            dtdt=self._locals.driver_tencencies.dtdt,
+            dudt=self._locals.driver_tencencies.dudt,
+            dvdt=self._locals.driver_tencencies.dvdt,
             liquid_precip_flux=state.non_anvil_large_scale.liquid_precip_flux,
             ice_precip_flux=state.non_anvil_large_scale.ice_precip_flux,
             evaporation=state.non_anvil_large_scale.evaporation,
@@ -309,16 +311,16 @@ class GFDL1M:
             radiation_rain=state.radiation_field.rain,
             radiation_snow=state.radiation_field.snow,
             radiation_graupel=state.radiation_field.graupel,
-            dcloud_fraction_dt=locals.driver_tencencies.dcloudfractiondt,
-            dtdt=locals.driver_tencencies.dtdt,
-            dudt=locals.driver_tencencies.dudt,
-            dvdt=locals.driver_tencencies.dvdt,
-            dicedt=locals.driver_tencencies.dicedt,
-            dliquiddt=locals.driver_tencencies.dliquiddt,
-            dvapordt=locals.driver_tencencies.dvapordt,
-            draindt=locals.driver_tencencies.draindt,
-            dsnowdt=locals.driver_tencencies.dsnowdt,
-            dgraupeldt=locals.driver_tencencies.dgraupeldt,
+            dcloud_fraction_dt=self._locals.driver_tencencies.dcloudfractiondt,
+            dtdt=self._locals.driver_tencencies.dtdt,
+            dudt=self._locals.driver_tencencies.dudt,
+            dvdt=self._locals.driver_tencencies.dvdt,
+            dicedt=self._locals.driver_tencencies.dicedt,
+            dliquiddt=self._locals.driver_tencencies.dliquiddt,
+            dvapordt=self._locals.driver_tencencies.dvapordt,
+            draindt=self._locals.driver_tencencies.draindt,
+            dsnowdt=self._locals.driver_tencencies.dsnowdt,
+            dgraupeldt=self._locals.driver_tencencies.dgraupeldt,
         )
 
         self._finalize(
@@ -374,8 +376,8 @@ class GFDL1M:
             dvdt_macro=state.tendencies.dvdt_macro,
             draindt_macro=state.tendencies.draindt_macro,
             dtdt_friction_pressure_weighted=state.tendencies.dtdt_friction_pressure_weighted,
-            local_p_mb=locals.p_mb,
-            local_mass=locals.mass,
-            local_u_unmodified=locals.u_unmodified,
-            local_v_unmodified=locals.v_unmodified,
+            local_p_mb=self._locals.p_mb,
+            local_mass=self._locals.mass,
+            local_u_unmodified=self._locals.u_unmodified,
+            local_v_unmodified=self._locals.v_unmodified,
         )
