a74573e54624351f05c8068b8b8208c101900c12
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-01-26 11:49:09 -0500

    added gate sounding error

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/cumulus_parameterization/sounding/sounding.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/cumulus_parameterization/sounding/sounding.py
@@ -15,8 +15,13 @@ class Sounding:
 
 
 class GATESounding:
-    def __init__(self):
-        pass
+    def __init__(self, cumulus_parameterization_config: GF2020CumulusParameterizationConfig):
+        if cumulus_parameterization_config.WRTGRADS == 1:
+            raise NotImplementedError(
+                "GATE Sounding capabilities have not been implemented. You should have been caught"
+                "before getting here by the config checker. Beware, something likely failing in the config"
+                "checker as well - you may be unknowingly calling other untested/unimplemented sections."
+            )
 
     def __call__(self, *args, **kwds):
         pass
