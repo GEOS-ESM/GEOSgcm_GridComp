61d198d17aec64ec41d0a9b3aba81ddc8a592332
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-02-02 18:08:00 -0500

    New field types for Convection Tracers

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/field_types.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/field_types.py
@@ -1,11 +1,12 @@
 from ndsl.dsl.gt4py import IJK, IJ, Field, GlobalTable
-from ndsl.dsl.typing import Float
+from ndsl.dsl.typing import Float, Int, Bool
 from pyMoist.constants import N_MODES, NCNST
 
 
 FloatField_NModes = Field[IJK, (Float, (N_MODES))]
 FloatField_NTracers = Field[IJK, (Float, (int(NCNST)))]
 FloatFieldIJ_NTracers = Field[IJ, (Float, (int(NCNST)))]
-TracerMetaDataTable = GlobalTable[(Float, int(NCNST))]
-TracerMetaDataTable_extra_dim_3 = GlobalTable[(Float, (int(NCNST), 3))]
-TracerMetaDataTable_extra_dim_4 = GlobalTable[(Float, (int(NCNST), 4))]
+ConvectionTracerMetaDataTable_Float = GlobalTable[(Float, int(NCNST))]
+ConvectionTracerMetaDataTable_Bool = GlobalTable[(Bool, int(NCNST))]
+ConvectionTracerMetaDataTable_x3 = GlobalTable[(Float, (int(NCNST), 3))]
+ConvectionTracerMetaDataTable_x4 = GlobalTable[(Float, (int(NCNST), 4))]
