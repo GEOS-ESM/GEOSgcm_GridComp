65f9768512d555febfb7b1847d8ba2333c72cc83
Florian Deconinck <deconinck.florian@gmail.com>
2026-02-12 12:11:37 -0500

    Saturation tables [UNTESTED]

---
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/saturation_tables/__init__.py
@@ -0,0 +1,22 @@
+from pyMoist.saturation_tables.types import GlobalTable_saturation_tables  # isort: skip
+from pyMoist.saturation_tables.formulation import SaturationFormulation  # isort: skip
+from pyMoist.saturation_tables.qsat_functions import (
+    saturation_specific_humidity,
+    saturation_specific_humidity_frozen_surface,
+    saturation_specific_humidity_liquid_surface,
+)
+from pyMoist.saturation_tables.tables.main import (
+    SaturationVaporPressureTable,
+    get_saturation_vapor_pressure_table,
+)
+
+
+__all__ = [
+    "GlobalTable_saturation_tables",
+    "SaturationVaporPressureTable",
+    "SaturationFormulation",
+    "saturation_specific_humidity",
+    "saturation_specific_humidity_frozen_surface",
+    "saturation_specific_humidity_liquid_surface",
+    "get_saturation_vapor_pressure_table",
+]
