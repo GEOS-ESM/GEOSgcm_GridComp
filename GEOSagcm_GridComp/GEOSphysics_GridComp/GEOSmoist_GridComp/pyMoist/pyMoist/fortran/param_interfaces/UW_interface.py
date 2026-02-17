921d33a09cca11f9b2ab966162fc1593f7c4f9d7
Florian Deconinck <deconinck.florian@gmail.com>
2026-02-17 12:21:29 -0500

    Move UW interface to Managed State

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/fortran/param_interfaces/UW_interface.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/fortran/param_interfaces/UW_interface.py
@@ -1,14 +1,20 @@
 import numpy as np
 from ndsl.dsl.typing import Float, Int
+from ndsl.constants import I_DIM, J_DIM
 from pyGEOSBridge import GEOSInterfaceCode, get_MAPLPy
 
+from mpi4py import MPI
+
+from pyMoist.fortran.build_helper import InterfaceTransferType, StencilBackendCompilerOverride
 from pyMoist.fortran import get_NDSL_physics
+from pyMoist.fortran.managed_state import MAPLManagedState
+from pyMoist.fortran.memory_factory import MAPLMemoryRepository
 from pyMoist.UW import ComputeUwshcuInv, UWConfiguration, UWState
 
 
 class UWGEOSInterface(GEOSInterfaceCode):
     def __init__(self, name: str) -> None:
-        self._state = None
+        self._state: UWState | None = None
 
     def init(self, mapl_state, import_state, export_state) -> None:
         # Make sure we have our NDSL stack setup
@@ -17,6 +23,10 @@ class UWGEOSInterface(GEOSInterfaceCode):
 
         # Init NDSL state
         self._state = UWState.empty(ndsl_stack.quantity_factory)
+        self._managed_state = MAPLManagedState(
+            UWState.empty(ndsl_stack.quantity_factory),
+            InterfaceTransferType.CPU_MAP,
+        )
 
         if ndsl_stack.quantity_factory.sizer.nz == 72:
             jason_uw = MAPLPy.get_resource("JASON_UW:", mapl_state, default=True)
@@ -64,117 +74,90 @@ class UWGEOSInterface(GEOSInterfaceCode):
         )
 
         # Build UW
-        self._uw = ComputeUwshcuInv(ndsl_stack.stencil_factory, ndsl_stack.quantity_factory, config)
+        with StencilBackendCompilerOverride(
+            MPI.COMM_WORLD,
+            ndsl_stack.stencil_factory.config.dace_config,
+        ):
+            self._uw = ComputeUwshcuInv(ndsl_stack.stencil_factory, ndsl_stack.quantity_factory, config)
 
     def run(self, mapl_state, import_state, export_state) -> None:
         raise RuntimeError("UW requires pyMoist integration requires `run_with_internal`")
 
-    def run_with_internal(self, mapl_state, import_state, export_state, internal_state) -> None:
-        MAPLPy = get_MAPLPy()
+    def __run_with_internal(self, mapl_state, import_state, export_state, internal_state) -> None:
+        assert self._state is not None
 
-        # Internals
-        Q = MAPLPy.get_pointer("Q", internal_state, dtype=np.float32)  # ??
-        self._state.input_output.qv0_inv.data[:] = Q
-        QLLS = MAPLPy.get_pointer("QLLS", internal_state, dtype=np.float32)
-        self._state.input.QLLS.data[:] = QLLS
-        QLCN = MAPLPy.get_pointer("QLCN", internal_state, dtype=np.float32)
-        self._state.input.QLCN.data[:] = QLCN
-        CLCN = MAPLPy.get_pointer("CLCN", internal_state, dtype=np.float32)  # ??
-        CLLS = MAPLPy.get_pointer("CLLS", internal_state, dtype=np.float32)  # ❌
-        QILS = MAPLPy.get_pointer("QILS", internal_state, dtype=np.float32)
-        self._state.input.QILS.data[:] = QILS
-        QICN = MAPLPy.get_pointer("QICN", internal_state, dtype=np.float32)
-        self._state.input.QICN.data[:] = QICN
-        CUSH = MAPLPy.get_pointer("CUSH", internal_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2])
-        self._state.input_output.cush.data[:] = CUSH
+        ndsl_stack = get_NDSL_physics(mapl_state)
+        import_repository = MAPLMemoryRepository(import_state, ndsl_stack.quantity_factory)
+        internal_repository = MAPLMemoryRepository(internal_state, ndsl_stack.quantity_factory)
+        export_repository = MAPLMemoryRepository(export_state, ndsl_stack.quantity_factory)
 
-        # Imports
-        FRLAND = MAPLPy.get_pointer("FRLAND", import_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2])
-        self._state.input.frland.data[:] = FRLAND
-        ZLE = MAPLPy.get_pointer("ZLE", import_state, dtype=np.float32)
-        self._state.input.ZLE.data[:] = ZLE
-        PLE = MAPLPy.get_pointer("PLE", import_state, dtype=np.float32)
-        self._state.input.PLE.data[:] = PLE
-        T = MAPLPy.get_pointer("T", import_state, dtype=np.float32)
-        self._state.input_output.t0_inv.data[:] = T
-        U = MAPLPy.get_pointer("U", import_state, dtype=np.float32)
-        self._state.input_output.u0_inv.data[:] = U
-        V = MAPLPy.get_pointer("V", import_state, dtype=np.float32)
-        self._state.input_output.v0_inv.data[:] = V
-        SH = MAPLPy.get_pointer("SH", import_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2])
-        self._state.input.shfx.data[:] = SH
-        EVAP = MAPLPy.get_pointer("EVAP", import_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2])
-        self._state.input.evap.data[:] = EVAP
-        KPBL_SC = MAPLPy.get_pointer("KPBL_SC", import_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2])
-        self._state.input.kpbl_inv.data[:] = KPBL_SC
-        TKE = MAPLPy.get_pointer("TKE", import_state, dtype=np.float32)
-        self._state.input.tke_inv.data[:] = TKE
+        self._managed_state.register("input.PLE", "PLE", import_repository)
+        self._managed_state.register("input.ZLE", "ZLE", import_repository)
+        self._managed_state.register("input.QLLS", "QLLS", internal_repository)
+        self._managed_state.register("input.QILS", "QILS", internal_repository)
+        self._managed_state.register("input.QLCN", "QLCN", internal_repository)
+        self._managed_state.register("input.QICN", "QICN", internal_repository)
+        self._managed_state.register("input.kpbl_inv", "KPBL_SC", import_repository, dims=[I_DIM, J_DIM])
+        self._managed_state.register("input.frland", "FRLAND", import_repository, dims=[I_DIM, J_DIM])
+        self._managed_state.register("input.tke_inv", "TKE", import_repository)
+        self._managed_state.register("input.shfx", "SH", import_repository, dims=[I_DIM, J_DIM])
+        self._managed_state.register("input.evap", "EVAP", import_repository, dims=[I_DIM, J_DIM])
 
-        # Required Exports (connectivities to moist siblings)
-        MFD_SC = MAPLPy.get_pointer("MFD_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.MFD_SC.data[:] = MFD_SC
-        QLDET_SC = MAPLPy.get_pointer("QLDET_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qldet_inv.data[:] = QLDET_SC
-        QIDET_SC = MAPLPy.get_pointer("QIDET_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qidet_inv.data[:] = QIDET_SC
-        CUFRC_SC = MAPLPy.get_pointer("CUFRC_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.cufrc_inv.data[:] = CUFRC_SC
-        CNPCPRATE = MAPLPy.get_pointer("CNPCPRATE", export_state, dtype=np.float32, alloc=True)
-        self._state.input_output.cnvtr.data[:] = CNPCPRATE
-        CNV_FRC = MAPLPy.get_pointer("CNV_FRC", export_state, dtype=np.float32, alloc=True)
-        SRF_TYPE = MAPLPy.get_pointer("SRF_TYPE", export_state, dtype=np.float32, alloc=True)
-        # Tendency Export
-        DUDT_SC = MAPLPy.get_pointer("DUDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.uten_inv.data[:] = DUDT_SC
-        DVDT_SC = MAPLPy.get_pointer("DVDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.vten_inv.data[:] = DVDT_SC
-        DTDT_SC = MAPLPy.get_pointer("DTDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.tten_inv.data[:] = DTDT_SC
-        DQVDT_SC = MAPLPy.get_pointer("DQVDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qvten_inv.data[:] = DQVDT_SC
-        DQIDT_SC = MAPLPy.get_pointer("DQIDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qiten_inv.data[:] = DQIDT_SC
-        DQLDT_SC = MAPLPy.get_pointer("DQLDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qlten_inv.data[:] = DQLDT_SC
-        DQRDT_SC = MAPLPy.get_pointer("DQRDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qrten_inv.data[:] = DQRDT_SC
-        DQSDT_SC = MAPLPy.get_pointer("DQSDT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qsten_inv.data[:] = DQSDT_SC
-        DQADT_SC = MAPLPy.get_pointer("DQADT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.DQADT_SC.data[:] = DQADT_SC
-        # Exports
-        UMF_SC = MAPLPy.get_pointer("UMF_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.umf_inv.data[:] = UMF_SC
-        DCM_SC = MAPLPy.get_pointer("DCM_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.dcm_inv.data[:] = DCM_SC
-        ENTR_SC = MAPLPy.get_pointer("ENTR_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.fer_inv.data[:] = ENTR_SC
-        DETR_SC = MAPLPy.get_pointer("DETR_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.fdr_inv.data[:] = DETR_SC
-        QLSUB_SC = MAPLPy.get_pointer("QLSUB_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qlsub_inv.data[:] = QLSUB_SC
-        QISUB_SC = MAPLPy.get_pointer("QISUB_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qisub_inv.data[:] = QISUB_SC
-        SC_NDROP = MAPLPy.get_pointer("SC_NDROP", export_state, dtype=np.float32, alloc=True)
-        self._state.output.ndrop_inv.data[:] = SC_NDROP
-        SC_NICE = MAPLPy.get_pointer("SC_NICE", export_state, dtype=np.float32, alloc=True)
-        self._state.output.nice_inv.data[:] = SC_NICE
-        TPERT_SC = MAPLPy.get_pointer("TPERT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.tpert_out.data[:] = TPERT_SC
-        QPERT_SC = MAPLPy.get_pointer("QPERT_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qpert_out.data[:] = QPERT_SC
-        QTFLX_SC = MAPLPy.get_pointer("QTFLX_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.qtflx_inv.data[:] = QTFLX_SC
-        SLFLX_SC = MAPLPy.get_pointer("SLFLX_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.slflx_inv.data[:] = SLFLX_SC
-        UFLX_SC = MAPLPy.get_pointer("UFLX_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.uflx_inv.data[:] = UFLX_SC
-        VFLX_SC = MAPLPy.get_pointer("VFLX_SC", export_state, dtype=np.float32, alloc=True)
-        self._state.output.vflx_inv.data[:] = VFLX_SC
-        RKFRE = MAPLPy.get_pointer(
-            "RKFRE", export_state, dtype=np.float32, dims=MAPLPy.grid_dims[0:2], alloc=True
+        self._managed_state.register("input_output.u0_inv", "U", import_repository)
+        self._managed_state.register("input_output.v0_inv", "V", import_repository)
+        self._managed_state.register("input_output.qv0_inv", "Q", internal_repository)
+        self._managed_state.register("input_output.t0_inv", "T", import_repository)
+        self._managed_state.register("input_output.cush", "CUSH", internal_repository)
+        # self._managed_state.register("input_output.CNV_Tracers", ) # ❔❔❔
+        self._managed_state.register("input_output.cnvtr", "CNPCPRATE", export_repository)
+
+        self._managed_state.register(
+            "output.RKFRE", "RKFRE", export_repository, alloc=True, dims=[I_DIM, J_DIM]
         )
-        self._state.output.RKFRE.data[:] = RKFRE
+        self._managed_state.register("output.MFD_SC", "MFD_SC", export_repository, alloc=True)
+        self._managed_state.register("output.QLENT_SC", "QLENT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.QIENT_SC", "QIENT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.umf_inv", "UMF_SC", export_repository, alloc=True)
+        self._managed_state.register("output.dcm_inv", "DCM_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qtflx_inv", "QTFLX_SC", export_repository, alloc=True)
+        self._managed_state.register("output.slflx_inv", "SLFLX_SC", export_repository, alloc=True)
+        self._managed_state.register("output.uflx_inv", "UFLX_SC", export_repository, alloc=True)
+        self._managed_state.register("output.vflx_inv", "VFLX_SC", export_repository, alloc=True)
+        self._managed_state.register("output.DQADT_SC", "DQADT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qvten_inv", "DQVDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qlten_inv", "DQLDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qiten_inv", "DQIDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.tten_inv", "DTDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.uten_inv", "DUDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.vten_inv", "DVDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qrten_inv", "DQRDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qsten_inv", "DQSDT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.cufrc_inv", "CUFRC_SC", export_repository, alloc=True)
+        self._managed_state.register("output.fer_inv", "ENTR_SC", export_repository, alloc=True)
+        self._managed_state.register("output.fdr_inv", "DETR_SC", export_repository, alloc=True)
+        self._managed_state.register("output.ndrop_inv", "SC_NDROP", export_repository, alloc=True)
+        self._managed_state.register("output.nice_inv", "SC_NICE", export_repository, alloc=True)
+        self._managed_state.register("output.qlsub_inv", "QLSUB_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qisub_inv", "QISUB_SC", export_repository, alloc=True)
+        self._managed_state.register("output.tpert_out", "TPERT_SC", export_repository, alloc=True)
+        self._managed_state.register("output.qpert_out", "QPERT_SC", export_repository, alloc=True)
+
+        # Un-bound in DSL ?!
+        # self._managed_state.register("output.qidet_inv", "", export_repository, alloc=True)
+        # self._managed_state.register("output.qldet_inv", "", export_repository, alloc=True)
+        # self._managed_state.register("output.CNV_MFC", "", export_repository, alloc=True)
+        # self._managed_state.register("output.CNV_MFD", "", export_repository, alloc=True)
+        # self._managed_state.register("output.SHLW_PRC3", "", export_repository, alloc=True)
+        # self._managed_state.register("output.SHLW_SNO3", "", export_repository, alloc=True)
+        # self._managed_state.register("output.SC_QT", "", export_repository, alloc=True)
+        # self._managed_state.register("output.SC_MSE", "", export_repository, alloc=True)
+        # self._managed_state.register("output.CUSH_SC", "", export_repository, alloc=True)
+
+        # Unused from GEOS ?!
+        # CLCN = MAPLPy.get_pointer("CLCN", internal_state, dtype=np.float32)
+        # CLLS = MAPLPy.get_pointer("CLLS", internal_state, dtype=np.float32)
+        # CNV_FRC = MAPLPy.get_pointer("CNV_FRC", export_state, dtype=np.float32, alloc=True)
+        # SRF_TYPE = MAPLPy.get_pointer("SRF_TYPE", export_state, dtype=np.float32, alloc=True)
 
         self._uw(self._state)
 
