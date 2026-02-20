b15fa844b08c20a261b97976d197f2aaf7cac489
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-02-20 15:07:37 -0500

    GF2020 translate test

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/GF_2020.py
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/pyMoist/pyMoist/convection/GF_2020/GF_2020.py
@@ -1,10 +1,20 @@
 import copy
-from pyMoist.convection.GF_2020.config import GF2020Config
 from ndsl import StencilFactory, QuantityFactory
+from pyMoist.saturation_tables.tables.main import SaturationVaporPressureTable
+from pyMoist.convection.GF_2020.config import GF2020Config
+from pyMoist.convection.GF_2020.cumulus_parameterization.cumulus_parameterization import GF2020Config
+from pyMoist.convection.GF_2020.state import GF2020State
 from pyMoist.convection.GF_2020.locals import GF2020Locals
 from pyMoist.saturation_tables.tables.main import SaturationVaporPressureTable
-from pyMoist.convection.GF_2020.state import GF2020State
+from pyMoist.convection.GF_2020.cumulus_parameterization.state import GF2020CumulusParameterizationState
+from pyMoist.convection_tracers import ConvectionTracers
+from pyMoist.convection.GF_2020.cumulus_parameterization.config import GF2020CumulusParameterizationConfig
+import pyMoist.convection.GF_2020.cumulus_parameterization.constants as cumulus_parameterization_constants
 from pyMoist.convection.GF_2020.setup import GF2020Setup
+from pyMoist.convection.GF_2020.cumulus_parameterization.cumulus_parameterization import (
+    GF2020CumulusParameterization,
+)
+from pyMoist.convection.GF_2020.finalize import GF2020Finalize
 
 
 class GF2020:
@@ -12,31 +22,81 @@ class GF2020:
         self,
         stencil_factory: StencilFactory,
         quantity_factory: QuantityFactory,
-        GF_2020_config: GF2020Config,
+        config: GF2020Config,
+        cumulus_parameterization_config: GF2020CumulusParameterizationConfig,
+        saturation_tables: SaturationVaporPressureTable | None,
     ):
-        # Create extra quantity factories
-        self.nmp_quantity_factory = self.make_nmp_quantity_factory(quantity_factory)
+        # make saturation tables visible at runtime
+        if saturation_tables is None:
+            saturation_tables = SaturationVaporPressureTable(stencil_factory.backend)
+        else:
+            self.saturation_tables = saturation_tables
 
-        # initialize saturation tables
-        self.saturation_tables = SaturationVaporPressureTable(self.stencil_factory.backend)
-
-        # initialize extra quantities
-        self.temporaries = GF2020Locals.make(self.quantity_factory)
-
-        # initialize submodules and build stencils
-        self.setup = GF2020Setup(stencil_factory, quantity_factory, GF_2020_config)
-
-    @staticmethod
-    def make_nmp_quantity_factory(
-        quantity_factory: QuantityFactory,
-    ):
-        nmp_quantity_factory = copy.deepcopy(quantity_factory)
-        nmp_quantity_factory.set_extra_dim_lengths(
-            **{
-                "nmp": 2,
-            }
+        # initialize GF2020 locals
+        self.locals = GF2020Locals.zeros(
+            quantity_factory,
+            data_dimensions={
+                "plumes": 3,
+                "convection_tracers": config.NUMBER_OF_TRACERS,
+            },
         )
-        return nmp_quantity_factory
 
-    def __call__(self, p_interface, GF_2020_state: GF2020State):
-        self.setup()
+        # initialize GF2020 CumulusParameterization state
+        self.cumulus_parameterization_state = GF2020CumulusParameterizationState.zeros(
+            quantity_factory,
+            data_dimensions={
+                "plumes": cumulus_parameterization_constants.NUMBER_OF_PLUMES,
+                "convection_tracers": config.NUMBER_OF_TRACERS,
+            },
+        )
+
+        # initialize submodules
+        self._setup = GF2020Setup(
+            stencil_factory=stencil_factory,
+            quantity_factory=quantity_factory,
+            config=config,
+            saturation_tables=saturation_tables,
+        )
+
+        self._cumulus_parameterization_core = GF2020CumulusParameterization(
+            stencil_factory=stencil_factory,
+            quantity_factory=quantity_factory,
+            config=config,
+            cumulus_parameterization_config=cumulus_parameterization_config,
+            saturation_tables=saturation_tables,
+        )
+
+        self._finalize = GF2020Finalize(
+            stencil_factory=stencil_factory,
+            quantity_factory=quantity_factory,
+            config=config,
+            cumulus_parameterization_config=cumulus_parameterization_config,
+            saturation_tables=saturation_tables,
+        )
+
+    def __call__(self, state: GF2020State, convection_tracers: ConvectionTracers):
+        # flag to stop convection scheme for single column models
+        # this can be set in setup if surface temperature is very near zero Kelvin
+        scm_stop = False
+
+        # call the there parts of the scheme
+        self._setup(
+            state=state,
+            locals=self.locals,
+            cumulus_parameterization_state=self.cumulus_parameterization_state,
+            convection_tracers=convection_tracers,
+            scm_stop=scm_stop,
+        )
+
+        if scm_stop == False:
+            self._cumulus_parameterization_core(
+                state=self.cumulus_parameterization_state,
+                convection_tracers=convection_tracers,
+            )
+
+            self._finalize(
+                state=state,
+                locals=self.locals,
+                cumulus_parameterization_state=self.cumulus_parameterization_state,
+                convection_tracers=convection_tracers,
+            )
