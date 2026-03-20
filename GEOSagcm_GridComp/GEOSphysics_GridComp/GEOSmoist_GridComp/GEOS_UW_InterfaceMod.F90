5f1698bb662d0c2ee93c089ff158cdc635b7ceb2
Charles Kropiewnicki <charles.j.krop@gmail.com>
2026-03-20 12:39:32 -0400

    removing debug saves in UW

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/GEOS_UW_InterfaceMod.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSmoist_GridComp/GEOS_UW_InterfaceMod.F90
@@ -107,7 +107,6 @@ subroutine UW_Initialize (MAPL, CF, CLOCK, IMPORT, EXPORT, RC)
       call MAPL_ConfigSetAttribute(CF, UW_DT, 'DSL__UW_DT:', RC=STATUS); VERIFY_(STATUS)
       call MAPL_pybridge_gcinit( "pyMoist.fortran.param_interfaces.convection.UW_interface", MAPL, IMPORT, EXPORT )
     else
-      call MAPL_pybridge_gcinit( "pyMoist.fortran.param_interfaces.convection.UW_interface_NOOP", MAPL, IMPORT, EXPORT )
 
     call MAPL_GetResource(MAPL, USE_TRACER_TRANSP_UW,        'USE_TRACER_TRANSP_UW:',default= 1      , RC=STATUS) ; VERIFY_(STATUS)
     if (LM==72) then
@@ -248,7 +247,6 @@ subroutine UW_Run (GC, IMPORT, EXPORT, CLOCK, RC)
       call CNV_Tracers_To_AOS()
     else
       call CNV_Tracers_To_SOA()
-      call MAPL_pybridge_gcrun_with_internal( "pyMoist.fortran.param_interfaces.convection.UW_interface_NOOP", MAPL, IMPORT, EXPORT, INTERNAL )
 
     ! Internals
     call MAPL_GetPointer(INTERNAL, Q,      'Q'       , RC=STATUS); VERIFY_(STATUS)
@@ -460,7 +458,6 @@ subroutine UW_Run (GC, IMPORT, EXPORT, CLOCK, RC)
         if (associated(PTR2D)) PTR2D = CUSH
   
         call CNV_Tracers_To_SOA()
-        call MAPL_pybridge_gcrun( "pyMoist.fortran.param_interfaces.convection.UW_interface_NOOP", MAPL, IMPORT, EXPORT )
 
     endif ! USE_PYMOIST_UW
 
@@ -487,8 +484,6 @@ subroutine UW_Finalize(gc, import, export, rc)
 
   if (USE_PYMOIST_UW) then
     call MAPL_pybridge_gcfinalize( "pyMoist.fortran.param_interfaces.convection.UW_interface", MAPL, IMPORT, EXPORT )
-  else
-    call MAPL_pybridge_gcfinalize( "pyMoist.fortran.param_interfaces.convection.UW_interface_NOOP", MAPL, IMPORT, EXPORT )
   endif
 
 end subroutine UW_Finalize
