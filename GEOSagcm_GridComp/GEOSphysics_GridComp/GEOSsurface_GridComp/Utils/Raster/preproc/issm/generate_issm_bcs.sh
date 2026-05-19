bdf00dd3761335cf06b64e95e492431ad9d82033
Aaron Stubblefield <aaron.g.stubblefield@nasa.gov>
2026-05-19 12:36:06 -0400

    slurm-ify issm bcs script

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/issm/generate_issm_bcs.sh
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSsurface_GridComp/Utils/Raster/preproc/issm/generate_issm_bcs.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=preproc_issm
+#SBATCH --time=0:20:00
+#SBATCH --ntasks=126
+#SBATCH --qos=debug
+#SBATCH --constraint=mil
+
 find . -mindepth 1 -type d ! -name utils_issm -exec bash -c '
 for dir do
 (
@@ -6,15 +13,21 @@ for dir do
     name=$(basename "$dir")
     
     for f in "${name}_meshgen.py" "${name}_parameterize.py" "${name}_control.py" "${name}_finalize.py"; do
-    [[ -f "$f" ]] || exit
+        [[ -f "$f" ]] || exit
     done
 
     source "$hdir/issm_env"
-    rm -f ISSM_${name}.bin ISSM_${name}.outbin ISSM_${name}.outlog ISSM_${name}.errlog
+    rm -f ISSM_${name}.bin ISSM_${name}.outbin ISSM_${name}.errlog 
     rm -rf netcdfs
     (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python ${name}_meshgen.py)
     (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python ${name}_control.py)
-    ${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) ISSM_${name} 2>> ISSM_${name}.errlog
+    
+    if [ -n "$SLURM_JOB_ID" ]; then
+        mpirun -np $SLURM_NTASKS ${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) ISSM_${name} 2>> ISSM_${name}.errlog
+    else
+        ${ISSM_DIR}/bin/issm.exe StressbalanceSolution $(pwd) ISSM_${name} 2>> ISSM_${name}.errlog
+    fi
+    
     (export LD_LIBRARY_PATH=$PYTHON_LIB:$LD_LIBRARY_PATH; python ${name}_finalize.py)
 )
 done
@@ -26,7 +39,7 @@ domain_name=$(
   python ./utils_issm/domain_name.py
 )
 
-mkdir "$domain_name"
+rm -rf "$domain_name" && mkdir "$domain_name"
 
 find . -type f -name "*.bin" -exec cp -t "$domain_name" {} +
 find . -type f -name "*.toolkits" -exec cp -t "$domain_name" {} +
