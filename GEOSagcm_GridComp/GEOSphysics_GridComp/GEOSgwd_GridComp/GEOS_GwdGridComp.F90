47997237953ad809db6fa13cf7e7374a7cad821d
Tom Clune <thomas.l.clune@nasa.gov>
2026-06-09 14:18:01 -0400

    oops

--- GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSgwd_GridComp/GEOS_GwdGridComp.F90
+++ GEOSagcm_GridComp/GEOSphysics_GridComp/GEOSgwd_GridComp/GEOS_GwdGridComp.F90
@@ -30,7 +30,7 @@ module GEOS_GwdGridCompMod
    ! !USES:
 
    use esmf
-   use MAPL, only: MAPL_Verify, MAPL_Assert, MAPL_Return&
+   use MAPL, only: MAPL_Verify, MAPL_Assert, MAPL_Return
    use MAPL, only: MAPL_get_current_thread
    use MAPL, only : MAPL_get_num_threads
    use MAPL, only : MAPL_find_bounds
