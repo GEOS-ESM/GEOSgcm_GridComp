ae5347ebf3cae53793177f46e2e1eda7a29d40f5
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2026-08-26 08:53:43 -0400

    GEOShs regression: only compare results against IntelLLVM baselines

--- GEOSagcm_GridComp/GEOShs_GridComp/regression/run_case.cmake
+++ GEOSagcm_GridComp/GEOShs_GridComp/regression/run_case.cmake
@@ -13,7 +13,7 @@ function(run_case case_name regression_data_dir)
   run_geos(${num_procs} ${case_name} ${expdir})
   if (NOT EXISTS ${checkpoints_dir})
     message(WARNING "Baseline directory [${checkpoints_dir}] not found. Skipping comparison.")
-  else()
+  elseif(FORTRAN_COMPILER_ID STREQUAL "IntelLLVM") # only compare against IntelLLVM baselines
     compare_results(${checkpoints_dir} ${expdir}/checkpoints/last)
   endif()
 
